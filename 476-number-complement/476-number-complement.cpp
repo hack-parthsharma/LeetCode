47ff65ae3436d656167cdcb77f4be41514d3a436
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-02-27 15:49:30 +0530

    Time: 4 ms (10.97%), Space: 5.9 MB (95.00%) - LeetHub

---
+++ 476-number-complement/476-number-complement.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        int inv = 0, iter = 0;
+        int reminder;
+        while(num) {
+            inv = inv | ((1 - (num % 2))<<iter);
+            num /= 2;
+            iter++;
+        }
+        return inv;
+    }
+};
