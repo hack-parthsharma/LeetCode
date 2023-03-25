926ec63101371a92f33c9e2e83980e138082ec3b
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-03-25 16:36:59 +0530

    Time: 15 ms (91.29%), Space: 16.9 MB (88.56%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,20 +1,11 @@
-class Solution
-{
-	public:
-		int singleNumber(vector<int> &nums)
-		{
-			//initialised the xor of numbers in xorr variable
-			int xorr = 0;
-			//Linearly traversing the array
-			for (auto x = 0; x < nums.size(); x++)
-			{
-				//Calculating Xor of two numbers .
-				xorr = xorr ^ nums[x];
+class Solution {
+ public:
+  int singleNumber(vector<int>& nums) {
+    int ans = 0;
 
-			}
+    for (const int num : nums)
+      ans ^= num;
 
-			// Atlast return the final xor .
-
-			return xorr;
-		}
+    return ans;
+  }
 };
