974cc45be47c2f90fea176a7f3a188f10e7bf8ad
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-03-25 16:31:57 +0530

    Time: 7 ms (72.38%), Space: 13.1 MB (65.49%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,28 +1,14 @@
-class Solution
-{
-	public:
-		int maxProfit(vector<int> &prices)
-		{
-			int sum = 0;
-			//If the number of element in the array are zero or one just return zero.
-			if (prices.size() == 1 or prices.size() == 0)
-			{
-				return 0;
-			}
+class Solution {
+ public:
+  int maxProfit(vector<int>& prices) {
+    int sell = 0;
+    int hold = INT_MIN;
 
-			//Traverse the array and compare the consecutive two elements .
-			for (int i = 0; i < prices.size() - 1; i++)
-			{
-				// If first consecuitve element is less than second subtract both and add in the sum varibale. 
-				if (prices[i] < prices[i + 1])
-				{
-					sum += prices[i + 1] - prices[i];
+    for (const int price : prices) {
+      sell = max(sell, hold + price);
+      hold = max(hold, sell - price);
+    }
 
-				}
-			}
-
-			//Finally return the sum.
-			return sum;
-
-		}
+    return sell;
+  }
 };
