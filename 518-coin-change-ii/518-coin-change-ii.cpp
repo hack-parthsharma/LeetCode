da4d3921b43ba0ad99ab5057ca8c6b7147952d44
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-03-27 15:12:06 +0530

    Time: 7 ms (96.25%), Space: 7.2 MB (92.11%) - LeetHub

--- 518-coin-change-ii/518-coin-change-ii.cpp
+++ 518-coin-change-ii/518-coin-change-ii.cpp
@@ -1,42 +1,13 @@
 class Solution {
-public:
-    int change(int amount, vector<int>& coins) {
-        return change_dp(amount, coins);
-        return change_recursive(amount, coins); // Time Limit Error
-    }
-    
-    
-    int change_recursive(int amount, vector<int>& coins) {
-        int result = 0;
-        change_recursive_helper(amount, coins, 0, result);
-        return result;
-    }
-    
-    // the `idx` is used for remove the duplicated solutions.
-    void change_recursive_helper(int amount, vector<int>& coins, int idx, int& result) {
-        if (amount == 0) { 
-            result++; 
-            return;
-        }
-    
-        for ( int i = idx; i < coins.size(); i++ ) {
-            if (amount < coins[i]) continue;
-            change_recursive_helper(amount - coins[i], coins, i, result);
-        }
-        return;
-    }
-    
-    int change_dp(int amount, vector<int>& coins) {
-        vector<int> dp(amount+1, 0);
-        dp[0] = 1;
-        for (int i=0; i<coins.size(); i++) {
-            for(int n=1; n<=amount; n++) {
-                if (n >= coins[i]) {
-                    dp[n] += dp[n-coins[i]];
-                }
-            }
-        }
+ public:
+  int change(int amount, vector<int>& coins) {
+    vector<int> dp(amount + 1);
+    dp[0] = 1;
 
-        return dp[amount];
-    }
+    for (const int coin : coins)
+      for (int i = coin; i <= amount; ++i)
+        dp[i] += dp[i - coin];
+
+    return dp[amount];
+  }
 };
