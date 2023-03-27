985c6b2dab1c5d144e24a50713307795e0107138
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-03-27 10:50:40 +0530

    Time: 123 ms (57.00%), Space: 96.1 MB (98.99%) - LeetHub

--- 463-island-perimeter/463-island-perimeter.cpp
+++ 463-island-perimeter/463-island-perimeter.cpp
@@ -1,27 +1,19 @@
 class Solution {
-public:
-    int edge(vector<vector<int>> &grid, int x, int y) {
-        int edge = 0;
-        
-        if (x==0 || (x>0 && grid[x-1][y] == 0 ) ) edge++; //up
-        if (y==0 || (y>0 && grid[x][y-1] == 0 ) ) edge++; //left
-        if (x == grid.size() - 1 || 
-            (x < grid.size() - 1 && grid[x+1][y] == 0)) edge++; //down
-        if (y == grid[0].size() - 1 || 
-            (y < grid[0].size() - 1 && grid[x][y+1] == 0)) edge++; //right
-        
-        return edge;
-    }
-            
-    int islandPerimeter(vector<vector<int>>& grid) {
-        int perimeter = 0;
-        for(int i=0; i<grid.size(); i++) {
-            for(int j=0; j<grid[0].size(); j++) {
-                if (grid[i][j] == 1) {
-                    perimeter += edge (grid, i, j);
-                }
-            }
+ public:
+  int islandPerimeter(vector<vector<int>>& grid) {
+    int islands = 0;
+    int neighbors = 0;
+
+    for (int i = 0; i < grid.size(); ++i)
+      for (int j = 0; j < grid[0].size(); ++j)
+        if (grid[i][j]) {
+          ++islands;
+          if (i - 1 >= 0 && grid[i - 1][j])
+            ++neighbors;
+          if (j - 1 >= 0 && grid[i][j - 1])
+            ++neighbors;
         }
-        return perimeter;
-    }
+
+    return islands * 4 - neighbors * 2;
+  }
 };
