14085456a70e792a47f2e68ef834b4f9b9bc48ca
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-03-27 15:12:53 +0530

    Time: 3 ms (27.38%), Space: 6.1 MB (73.68%) - LeetHub

--- 520-detect-capital/520-detect-capital.cpp
+++ 520-detect-capital/520-detect-capital.cpp
@@ -1,13 +1,10 @@
 class Solution {
-public:
-    bool detectCapitalUse(string word) {
-        bool all_caps = 1;
-        bool all_small = 1;
-        bool first_letter_caps = isupper(word[0]);
-
-        for(int i=1; i<word.length() && (all_caps | all_small); i++) {
-            islower(word[i])? all_caps = 0: all_small = 0;
-        }
-        return (all_small | (first_letter_caps & all_caps));
-    }
+ public:
+  bool detectCapitalUse(string word) {
+    for (int i = 1; i < word.length(); ++i)
+      if (isupper(word[1]) != isupper(word[i]) ||
+          islower(word[0]) && isupper(word[i]))
+        return false;
+    return true;
+  }
 };
