f72bae469bc320a312668203fa7574800bf8a6fb
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-03-28 18:39:38 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (89.35%) - LeetHub

---
+++ 564-find-the-closest-palindrome/564-find-the-closest-palindrome.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string makePalindromic(string s) {
+        for (int i = 0, j = (int)s.length() - 1; i < j; i++, j--)
+            s[j] = s[i];
+        return s;
+    }
+    
+    string nearestPalindromic(string n) {
+        if (n == "0")
+            return "1";
+        
+        long long orgVal = stoll(n);
+    
+        // candidate #1 (ex: 123xx -> 12321, 123xxx -> 123321)
+        string res = makePalindromic(n);
+        long long resVal = stoll(res);
+        long long diff = abs(resVal - orgVal);
+    
+        long long scale = (long long)pow(10, (int)n.length() / 2);
+    
+        // candidate #2 (ex: 123xx -> 12221, 123xxx -> 122221, 100xx -> 9999)
+        string smaller = makePalindromic(to_string((orgVal / scale) * scale - 1));
+        // candidate #3 (ex: 123xx -> 12421, 123xxx -> 124421, 99xx -> 10001)
+        string bigger = makePalindromic(to_string((orgVal / scale) * scale + scale));
+
+        long long smallerVal = stoll(smaller);
+        if (diff == 0 || abs(orgVal - smallerVal) <= diff) {
+            res = smaller;
+            diff = abs(orgVal - smallerVal);
+        }
+
+        long long biggerVal = stoll(bigger);
+        if (abs(orgVal - biggerVal) < diff)
+            res = bigger;
+    
+        return res;
+    }
+};
