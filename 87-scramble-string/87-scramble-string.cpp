89f45832cc3ee6af96f123d76a00ae22780c224b
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-03-30 18:13:39 +0530

    Time: 53 ms (71.42%), Space: 10.4 MB (87.52%) - LeetHub

--- 87-scramble-string/87-scramble-string.cpp
+++ 87-scramble-string/87-scramble-string.cpp
@@ -1,36 +1,35 @@
 class Solution {
- public:
-  bool isScramble(string s1, string s2) {
-    if (s1 == s2)
-      return true;
-    if (s1.length() != s2.length())
-      return false;
-    const string hashedKey = s1 + '+' + s2;
-    if (const auto it = memo.find(hashedKey); it != cend(memo))
-      return it->second;
-
-    vector<int> count(128);
-
-    for (int i = 0; i < s1.length(); ++i) {
-      ++count[s1[i]];
-      --count[s2[i]];
+public:
+    bool isScramble(string s1, string s2) {
+        if (s1 == s2) {
+            return true;
+        }
+        if (!is_permutation(s1.begin(), s1.end(), s2.begin())) {
+            return false;
+        }
+        
+        int n = s1.length();
+        vector<vector<vector<bool>>> dp(n, vector<vector<bool>>(n, vector<bool>(n+1, false)));
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                dp[i][j][1] = (s1[i] == s2[j]);
+            }
+        }
+        
+        for (int length = 2; length <= n; length++) {
+            for (int i = 0; i <= n-length; i++) {
+                for (int j = 0; j <= n-length; j++) {
+                    for (int k = 1; k < length; k++) {
+                        if ((dp[i][j][k] && dp[i+k][j+k][length-k]) || (dp[i][j+length-k][k] && dp[i+k][j][length-k])) {
+                            dp[i][j][length] = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        
+        return dp[0][0][n];
     }
-
-    if (any_of(begin(count), end(count), [](int c) { return c != 0; }))
-      return memo[hashedKey] = false;
-
-    for (int i = 1; i < s1.length(); ++i) {
-      if (isScramble(s1.substr(0, i), s2.substr(0, i)) &&
-          isScramble(s1.substr(i), s2.substr(i)))
-        return memo[hashedKey] = true;
-      if (isScramble(s1.substr(0, i), s2.substr(s2.length() - i)) &&
-          isScramble(s1.substr(i), s2.substr(0, s2.length() - i)))
-        return memo[hashedKey] = true;
-    }
-
-    return memo[hashedKey] = false;
-  }
-
- private:
-  unordered_map<string, bool> memo;
 };
