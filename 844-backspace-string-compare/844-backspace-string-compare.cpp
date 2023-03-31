22a820b6eca8bc3bfb57a9e71740172c061d6534
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-03-31 20:58:45 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (79.81%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -1,20 +1,17 @@
 class Solution {
-private:    
-    void removeBackspaces(string &s) {
-        int i = 0;
-        for(int i=0; i<s.size(); i++) {
-            if (s[i] == '#') {
-                int backSteps = i>0 ? 2 : 1;
-                s.erase(i-backSteps + 1, backSteps);
-                i -= backSteps;
-            } 
-        }
-    }
-    
-public:
-    bool backspaceCompare(string S, string T) {
-        removeBackspaces(S);
-        removeBackspaces(T);
-        return S == T;
-    }
+ public:
+  bool backspaceCompare(string s, string t) {
+    return backspace(s) == backspace(t);
+  }
+
+ private:
+  string backspace(const string& s) {
+    string stack;
+    for (const char c : s)
+      if (c != '#')
+        stack.push_back(c);
+      else if (!stack.empty())
+        stack.pop_back();
+    return stack;
+  }
 };
