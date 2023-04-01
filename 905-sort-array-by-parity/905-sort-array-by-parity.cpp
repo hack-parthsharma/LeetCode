7beb192681701fc94a3089a0a96499551c56e493
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-04-01 11:23:04 +0530

    Time: 16 ms (25.60%), Space: 16.2 MB (67.78%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.cpp
+++ 905-sort-array-by-parity/905-sort-array-by-parity.cpp
@@ -1,17 +1,18 @@
 class Solution {
-public:
-    bool isEven(int& x) {
-        return x % 2 == 0;
+ public:
+  vector<int> sortArrayByParity(vector<int>& nums) {
+    int l = 0;
+    int r = nums.size() - 1;
+
+    while (l < r) {
+      if (nums[l] % 2 == 1 && nums[r] % 2 == 0)
+        swap(nums[l], nums[r]);
+      if (nums[l] % 2 == 0)
+        ++l;
+      if (nums[r] % 2 == 1)
+        --r;
     }
-    vector<int> sortArrayByParity(vector<int>& A) {
-        //two pointer, one from left to right, another from right to left
-        // if left is odd number and right is even number, switch them
-        int l=0, r=A.size()-1;
-        while ( l < r ) {
-            if ( !isEven(A[l]) && isEven(A[r]) ) swap(A[l], A[r]);
-            if ( isEven(A[l]) ) l++;
-            if ( !isEven(A[r]) ) r--;
-        }
-        return A;
-    }
+
+    return nums;
+  }
 };
