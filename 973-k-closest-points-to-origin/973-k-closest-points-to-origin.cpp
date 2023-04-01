4cefc5e20a2fca25f943b2f3b5708aa0bc898c00
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-04-01 19:36:47 +0530

    Time: 187 ms (94.44%), Space: 59.2 MB (89.27%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -1,16 +1,32 @@
 class Solution {
-public:
-    vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
-        vector<pair<double,int>> vec;
-        for(int i=0;i<points.size();i++){//for loop for calculating distances and pushing it back in the vector
-            double distance = std::sqrt(std::pow(points[i][0], 2)+std::pow(points[i][1], 2));
-            vec.push_back(make_pair(distance,i));
-        }
-        sort(vec.begin(), vec.end());//sorting the vector according to distances
-        vector<vector<int>> f;//final points vector
-        for(int i=0;i<K;i++){
-            f.push_back(points[vec[i].second]);//pushing in the points accordingly
-        }
-        return f;
-    }
+ public:
+  vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+    quickSelect(points, 0, points.size() - 1, k);
+    return {begin(points), begin(points) + k};
+  };
+
+ private:
+  void quickSelect(vector<vector<int>>& points, int l, int r, int k) {
+    const int randIndex = rand() % (r - l + 1) + l;
+    swap(points[randIndex], points[r]);
+    const vector<int> pivot = points[r];
+
+    int nextSwapped = l;
+    for (int i = l; i < r; ++i)
+      if (squareDist(points[i]) <= squareDist(pivot))
+        swap(points[nextSwapped++], points[i]);
+    swap(points[nextSwapped], points[r]);
+
+    const int count = nextSwapped - l + 1;  // # of points <= pivot
+    if (count == k)
+      return;
+    if (count > k)
+      quickSelect(points, l, nextSwapped - 1, k);
+    else
+      quickSelect(points, nextSwapped + 1, r, k - count);
+  }
+
+  int squareDist(const vector<int>& p) {
+    return p[0] * p[0] + p[1] * p[1];
+  }
 };
