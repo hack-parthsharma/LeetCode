96fe9c2a09a8fc666228d101433b22045a1211e4
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-04-01 19:38:12 +0530

    Time: 27 ms (93.93%), Space: 26 MB (60.31%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
@@ -1,38 +1,16 @@
 class Solution {
-public:
-    vector<int> sortedSquares(vector<int>& A) {
-        // find the place, negative numbers are right, positive number are right.
-        // two pointer, one goes left, another goes right.
+ public:
+  vector<int> sortedSquares(vector<int>& nums) {
+    const int n = nums.size();
+    vector<int> ans(n);
+    int i = n - 1;
 
-        //using binary search algorithm
-        const int len = A.size();
-        int low = 0, high = len- 1;
-        int mid =0;
-        while (low <= high) {
-            mid = low + (high - low)/2;
-            if (A[mid] >= 0 ) high = mid - 1;
-            if (A[mid] < 0 ) low = mid + 1;
-        }
+    for (int l = 0, r = n - 1; l <= r;)
+      if (abs(nums[l]) > abs(nums[r]))
+        ans[i--] = nums[l] * nums[l++];
+      else
+        ans[i--] = nums[r] * nums[r--];
 
-        //TRICKY: make sure  A[mid] <= 0 or A[mid] is A[0]
-        if (A[mid] > 0 && mid > 0 ) mid--;
-        //cout << mid << " - "<< A[mid]<<  endl;
-
-        vector<int> result;
-        low = mid; high = mid+1;
-        while ( low >=0 && high < len ) {
-            if ( abs(A[low]) < abs(A[high]) ) {
-                result.push_back(A[low] * A[low]);
-                low --;
-            }else {
-                result.push_back(A[high] * A[high]);
-                high++;
-            }
-        }
-
-        for (;low >= 0; low--) result.push_back(A[low] * A[low]);
-        for (;high<len; high++) result.push_back(A[high] * A[high] );
-
-        return result;
-    }
+    return ans;
+  }
 };
