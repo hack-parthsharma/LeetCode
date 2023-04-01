0b5677d5de86ded7614b66a8a6c70f7678579a1b
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-04-01 19:43:30 +0530

    Time: 0 ms (100.00%), Space: 6 MB (8.57%) - LeetHub

--- 991-broken-calculator/991-broken-calculator.cpp
+++ 991-broken-calculator/991-broken-calculator.cpp
@@ -1,8 +1,16 @@
 class Solution {
-public:
-    int brokenCalc(int X, int Y) {
-        if (X >= Y) return X-Y ;
-        if ( Y%2 ==0 ) return brokenCalc(X, Y/2) + 1;
-        return brokenCalc(X, Y+1) + 1;
+ public:
+  int brokenCalc(int X, int Y) {
+    int ops = 0;
+
+    while (X < Y) {
+      if (Y % 2 == 0)
+        Y /= 2;
+      else
+        Y += 1;
+      ++ops;
     }
+
+    return ops + X - Y;
+  }
 };
