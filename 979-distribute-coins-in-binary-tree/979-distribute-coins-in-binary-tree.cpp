3630d1c9ddd50e3335a29322f4553bdb4b1ad716
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-04-01 19:38:56 +0530

    Time: 10 ms (22.36%), Space: 13.8 MB (87.75%) - LeetHub

--- 979-distribute-coins-in-binary-tree/979-distribute-coins-in-binary-tree.cpp
+++ 979-distribute-coins-in-binary-tree/979-distribute-coins-in-binary-tree.cpp
@@ -1,36 +1,20 @@
 class Solution {
-public:
-    int distributeCoins(TreeNode* root) {
-        int result = 0;
-        dfs(root, result);
-        return result;
-    }
+ public:
+  int distributeCoins(TreeNode* root) {
+    int ans = 0;
+    dfs(root, ans);
+    return ans;
+  }
 
-    //
-    // if a node has 0 coin, which means one move from its parent.
-    //               1 coin, which means zero move from its parent.
-    //               N coins, which means N-1 moves to its parent.
-    //
-    // So, we can simply know, the movement = coins -1.
-    // - negative number means the the coins needs be moved in.
-    // - positive number means the the coins nees be moved out.
-    //
-    // A node needs to consider the movement requests from both its left side and right side.
-    // and need to calculate the coins after left and right movement.
-    //
-    // So, the node coins  = my conins - the coins move out + the coins move in.
-    //
-    // Then we can have to code as below.
-    //
-    int dfs(TreeNode* root, int& result) {
-        if (root == NULL) return 0;
+  // Returns how many coins I can give (positive) / take (negative)
+ private:
+  int dfs(TreeNode* root, int& ans) {
+    if (root == nullptr)
+      return 0;
 
-        int left_move = dfs(root->left, result);
-        int right_move = dfs(root->right, result);
-        result += (abs(left_move) + abs(right_move));
-
-        // the coin after movement: coins = root->val +left_move + right_move
-        // the movement needs:  movement = coins - 1
-        return root->val + left_move + right_move - 1;
-    }
+    const int l = dfs(root->left, ans);
+    const int r = dfs(root->right, ans);
+    ans += abs(l) + abs(r);
+    return (root->val - 1) + l + r;
+  }
 };
