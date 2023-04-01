d16e075b15fcead01ee70883f7dd83481ce3c9a9
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-04-01 19:40:30 +0530

    Time: 4 ms (66.32%), Space: 9.7 MB (32.06%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
@@ -1,18 +1,20 @@
 class Solution {
-public:
-    int mincostTickets(vector<int>& days, vector<int>& costs) {
-        int dp[366] = {0};
-        unordered_set<int> travelDays;
-        for (int day : days) {
-            travelDays.insert(day);
-        }
-        for (int i = 1; i <= 365; i++) {
-            if (travelDays.find(i) == travelDays.end()) {
-                dp[i] = dp[i-1];
-            } else {
-                dp[i] = min(dp[i-1] + costs[0], min(dp[max(0, i-7)] + costs[1], dp[max(0, i-30)] + costs[2]));
-            }
-        }
-        return dp[365];
+ public:
+  int mincostTickets(vector<int>& days, vector<int>& costs) {
+    int ans = 0;
+    queue<pair<int, int>> last7;
+    queue<pair<int, int>> last30;
+
+    for (int day : days) {
+      while (!last7.empty() && last7.front().first + 7 <= day)
+        last7.pop();
+      while (!last30.empty() && last30.front().first + 30 <= day)
+        last30.pop();
+      last7.emplace(day, ans + costs[1]);
+      last30.emplace(day, ans + costs[2]);
+      ans = min({ans + costs[0], last7.front().second, last30.front().second});
     }
+
+    return ans;
+  }
 };
