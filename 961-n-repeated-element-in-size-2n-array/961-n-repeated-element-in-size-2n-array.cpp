192b5d7ca6c89a94bb35ca99ef0ab22a8a31da5d
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-04-01 19:26:42 +0530

    Time: 34 ms (80.42%), Space: 24.7 MB (91.63%) - LeetHub

--- 961-n-repeated-element-in-size-2n-array/961-n-repeated-element-in-size-2n-array.cpp
+++ 961-n-repeated-element-in-size-2n-array/961-n-repeated-element-in-size-2n-array.cpp
@@ -1,15 +1,9 @@
- class Solution {
-public:
-    int repeatedNTimes(vector<int>& A) {
-        map<int,int>myMap;
-        for(int i=0;i<A.size();i++){
-            myMap[A[i]]++;
-        }
-        for(auto x:myMap){
-            if((x.second)==(A.size()/2)){
-                return x.first;
-            }
-        }
-        return 0;
-    }
+class Solution {
+ public:
+  int repeatedNTimes(vector<int>& nums) {
+    for (int i = 0; i + 2 < nums.size(); ++i)
+      if (nums[i] == nums[i + 1] || nums[i] == nums[i + 2])
+        return nums[i];
+    return nums.back();
+  }
 };
