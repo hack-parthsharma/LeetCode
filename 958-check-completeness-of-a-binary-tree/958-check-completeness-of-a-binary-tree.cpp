686ddb920483ebcc7624a0b14d573e77f28ca749
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-04-01 19:25:36 +0530

    Time: 0 ms (100.00%), Space: 10.6 MB (12.43%) - LeetHub

--- 958-check-completeness-of-a-binary-tree/958-check-completeness-of-a-binary-tree.cpp
+++ 958-check-completeness-of-a-binary-tree/958-check-completeness-of-a-binary-tree.cpp
@@ -1,45 +1,21 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
+ public:
+  bool isCompleteTree(TreeNode* root) {
+    if (root == nullptr)
+      return true;
 
-public:
+    queue<TreeNode*> q{{root}};
 
-    bool isCompleteTree(TreeNode* root) {
-
-        queue<TreeNode*> q;
-
-        q.push(root);
-
-        while (q.front() != NULL) {
-
-            TreeNode* node = q.front();
-
-            q.pop();
-
-            q.push(node->left);
-
-            q.push(node->right);
-
-        }
-
-        while (!q.empty() && q.front() == NULL) {
-
-            q.pop();
-
-        }
-        return q.empty();
-
+    while (q.front() != nullptr) {
+      TreeNode* node = q.front();
+      q.pop();
+      q.push(node->left);
+      q.push(node->right);
     }
 
-};
-        
+    while (!q.empty() && q.front() == nullptr)
+      q.pop();
 
+    return q.empty();
+  }
+};
