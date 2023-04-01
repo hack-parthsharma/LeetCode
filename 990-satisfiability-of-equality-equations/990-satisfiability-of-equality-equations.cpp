25681ecce30b02e8dc8c909ef686ac9870934195
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-04-01 19:43:09 +0530

    Time: 11 ms (18.77%), Space: 11.4 MB (71.84%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
@@ -1,46 +1,41 @@
-class Solution {
-private:
-    map<char, char>  eq_map; // the table store the equivalent relationship
-    void init() {
-        for (char ch='a'; ch<='z'; ch++) {
-            eq_map[ch] = ch;
-        }
-    }
-public:
-    Solution() {
-        init();
-    }
-    
-    // find the tail node, 
-    // if a == b == c == d, find(a) would return d;
-    //                      find(b) would return d;
-    char find(char ch) {
-        while(ch != eq_map[ch]) {
-            ch = eq_map[ch];
-        }
-        return ch;
-    }
-    
-    //join x equivalent chain with y equivalent chain
-    void join(char x, char y) {
-        char tx = find(x);
-        char ty = find(y);
-        if (tx != ty) { 
-            eq_map[tx] = ty;
-        }
-    }
+class UnionFind {
+ public:
+  UnionFind(int n) : id(n) {
+    iota(begin(id), end(id), 0);
+  }
 
-    bool equationsPossible(vector<string>& equations) {
-        for (auto e : equations ) {
-            if (e[1] == '=' && e[0] != e[3]) { // equal
-                join(e[0], e[3]);
-            }
-        }
-        for (auto e : equations) {
-            if (e[1] == '!' && (e[0] == e[3] || find(e[0]) == find(e[3]) ) ) { 
-                return false;
-            }
-        }
-        return true;
-    }
+  void union_(int u, int v) {
+    id[find(u)] = find(v);
+  }
+
+  int find(int u) {
+    return id[u] == u ? u : id[u] = find(id[u]);
+  }
+
+ private:
+  vector<int> id;
+};
+
+class Solution {
+ public:
+  bool equationsPossible(vector<string>& equations) {
+    UnionFind uf(26);
+
+    for (const string& e : equations)
+      if (e[1] == '=') {
+        const int x = e[0] - 'a';
+        const int y = e[3] - 'a';
+        uf.union_(x, y);
+      }
+
+    for (const string& e : equations)
+      if (e[1] == '!') {
+        const int x = e[0] - 'a';
+        const int y = e[3] - 'a';
+        if (uf.find(x) == uf.find(y))
+          return false;
+      }
+
+    return true;
+  }
 };
