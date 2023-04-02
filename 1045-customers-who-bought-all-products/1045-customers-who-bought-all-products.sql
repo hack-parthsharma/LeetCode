cae002dfe3285db6d8524d3923dbcadb7481acc6
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-04-02 18:41:31 +0530

    Time: 970 ms (65.71%), Space: 0B (100.00%) - LeetHub

--- 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -1,6 +1,10 @@
-select customer_id
-from Customer
-group by customer_id
-having sum(distinct product_key) = (
-  select sum(product_key) from Product
-);
+select distinct customer_id
+    from (
+        select customer_id, count(distinct product_key) as product_count
+            from Customer
+            group by customer_id
+    ) as customer_product_counts
+    join (
+        select count(distinct product_key) as product_count from Product
+    ) as product_counts
+    on customer_product_counts.product_count = product_counts.product_count;
