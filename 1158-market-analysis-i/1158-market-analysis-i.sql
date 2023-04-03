a181d468fbfce4d27cc9b1ccd5d68e2de3de8a75
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-04-03 19:01:21 +0530

    Time: 2670 ms (21.70%), Space: 0B (100.00%) - LeetHub

---
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -0,0 +1,7 @@
+select user_id as buyer_id, join_date, 
+    sum(case when order_id is not null then 1 else 0 end) as orders_in_2019
+from users
+left join orders on users.user_id = orders.buyer_id 
+and year(order_date) = 2019
+group by user_id, join_date
+order by buyer_id
