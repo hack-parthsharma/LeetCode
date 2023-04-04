07206e33f9615622ebf6991efb3fe9ff7c3a323c
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-04-04 19:16:32 +0530

    Time: 8 ms (95.90%), Space: 11.7 MB (52.24%) - LeetHub

--- 1170-compare-strings-by-frequency-of-the-smallest-character/1170-compare-strings-by-frequency-of-the-smallest-character.cpp
+++ 1170-compare-strings-by-frequency-of-the-smallest-character/1170-compare-strings-by-frequency-of-the-smallest-character.cpp
@@ -1,49 +1,36 @@
 class Solution {
-public:
-    vector<int> numSmallerByFrequency(vector<string>& queries, vector<string>& words) {
-        cout << queries.size() << " : " << words.size() << endl;
-        vector<int> freq;
-        for (auto w : words) {
-            freq.push_back(f(w));
-        }
-        sort(freq.begin(), freq.end());
+ public:
+  vector<int> numSmallerByFrequency(vector<string>& queries,
+                                    vector<string>& words) {
+    vector<int> ans;
+    vector<int> wordsFreq;
 
-        vector<int> result;
-        for (auto q : queries) {
-            result.push_back(binary_search(freq, f(q)));
-        }
-        return result;
+    for (const string& word : words)
+      wordsFreq.push_back(f(word));
+    sort(begin(wordsFreq), end(wordsFreq));
+
+    for (const string& query : queries) {
+      const int freq = f(query);
+      ans.push_back(end(wordsFreq) -
+                    upper_bound(begin(wordsFreq), end(wordsFreq), freq));
     }
 
-    int f(string& s) {
-        char ch = 'z' + 1; //stroe the smallest char
-        int cnt = 0;  //stroe the frequency of the smallest char
-        for (auto c : s) {
-            if (c < ch) { //find the smaller char, reset the count
-                cnt  = 1;
-                ch = c;
-            } if (c == ch) {
-                cnt++;
-            }
-        }
-        return cnt;
-    }
+    return ans;
+  }
 
-    int binary_search(vector<int> &v, int target) {
-        int low=0, high=v.size()-1, mid;
+ private:
+  int f(const string& word) {
+    int count = 0;
+    char currentChar = 'z' + 1;
 
-        while (low < high) {
-            mid = low + (high - low) / 2;
-            if ( v[mid] > target) {
-                high = mid -1;
-            } else if (v[mid] <= target) {
-                low = mid + 1;
-            }
-        }
+    for (const char c : word)
+      if (c < currentChar) {
+        currentChar = c;
+        count = 1;
+      } else if (c == currentChar) {
+        ++count;
+      }
 
-        low = v[low] > target  ? low : low + 1;
-
-        return v.size() - low;
-    }
-
+    return count;
+  }
 };
