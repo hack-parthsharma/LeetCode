6d094d950f849dbb4f28389aed735d1718be376f
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-04-05 15:18:00 +0530

    Time: 7 ms (31.55%), Space: 8.3 MB (57.09%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,23 +1,16 @@
- class Solution {
-public:
-    bool uniqueOccurrences(vector<int>& arr) {
-        map<int,int>myMap;
-        for(int i=0;i<arr.size();i++){
-            myMap[arr[i]]++;
-        }
-        int temp=0;
-        vector<int>v;
-        for (auto i : myMap) {
-            v.push_back(i.second);
-        }
-        sort(v.begin(),v.end());
-        for(int i=0;i<v.size();i++){
-            if(v[i]==temp)
-                return false;
-            else
-                temp=v[i];
-        }
-        return true;
-             
-    }
+class Solution {
+ public:
+  bool uniqueOccurrences(vector<int>& arr) {
+    unordered_map<int, int> count;
+    unordered_set<int> occurrences;
+
+    for (const int a : arr)
+      ++count[a];
+
+    for (const auto& [_, value] : count)
+      if (!occurrences.insert(value).second)
+        return false;
+
+    return true;
+  }
 };
