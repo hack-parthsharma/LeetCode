4ab51e8cde3f5fa8f3cf311211a14c76a7461ca2
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-04-08 14:14:24 +0530

    Time: 101 ms (51.53%), Space: 28.4 MB (64.80%) - LeetHub

--- 1333-filter-restaurants-by-vegan-friendly-price-and-distance/1333-filter-restaurants-by-vegan-friendly-price-and-distance.cpp
+++ 1333-filter-restaurants-by-vegan-friendly-price-and-distance/1333-filter-restaurants-by-vegan-friendly-price-and-distance.cpp
@@ -1,22 +1,23 @@
-static bool cmp(const vector<int>& v1, const vector<int>& v2) {
-    return v1[1] == v2[1] ? v1[0] > v2[0] : v1[1] > v2[1];
-}
 class Solution {
-public:
-    vector<int> filterRestaurants(vector<vector<int>>& restaurants, int veganFriendly, int maxPrice, int maxDistance) {
-        vector<vector<int>> res;
-        
-        for(int i = 0; i < restaurants.size(); i++) {
-            if (!restaurants[i][2] && veganFriendly) continue;
-            if (restaurants[i][3] > maxPrice) continue;
-            if (restaurants[i][4] > maxDistance) continue;
-            res.push_back(restaurants[i]);
-        }
-        
-        sort(res.begin(), res.end(), cmp);
-        
-        vector<int> ids;
-        for(int i = 0; i < res.size(); i++) ids.push_back(res[i][0]);
-        return ids;        
-    }
+ public:
+  vector<int> filterRestaurants(vector<vector<int>>& restaurants,
+                                int veganFriendly, int maxPrice,
+                                int maxDistance) {
+    vector<int> ans;
+    vector<vector<int>> filtered;
+
+    for (vector<int>& restaurant : restaurants)
+      if (restaurant[2] >= veganFriendly && restaurant[3] <= maxPrice &&
+          restaurant[4] <= maxDistance)
+        filtered.push_back(restaurant);
+
+    sort(begin(filtered), end(filtered), [](const auto& a, const auto& b) {
+      return a[1] == b[1] ? a[0] > b[0] : a[1] > b[1];
+    });
+
+    for (const vector<int>& f : filtered)
+      ans.push_back(f[0]);
+
+    return ans;
+  }
 };
