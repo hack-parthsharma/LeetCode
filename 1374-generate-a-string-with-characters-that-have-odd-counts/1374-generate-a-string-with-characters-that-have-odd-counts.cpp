2ac6eb2a4c0ff00e53d7a75d782446dd7e8aa83f
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-04-08 19:24:34 +0530

    Time: 4 ms (42.92%), Space: 6.3 MB (61.81%) - LeetHub

---
+++ 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.cpp
@@ -0,0 +1,10 @@
+//Memory Usage: 6.7 MB, less than 100.00% of C++ online submissions for Generate a String With Characters That Have Odd Counts.
+class Solution {
+public:
+    string generateTheString(int n) {
+        if(n % 2 == 1) return string(n, 'a');
+        string ans = string(n-1, 'a');
+        ans += 'b';
+        return ans;
+    }
+};
