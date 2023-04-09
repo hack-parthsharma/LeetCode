b211dd8b9ea0a71f008515fda517b08df9262237
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-04-09 18:46:10 +0530

    Time: 1 ms (83.02%), Space: 42.8 MB (29.21%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {        // Initialize maximum element 
+         int max = candies[0];          // Traverse array elem to find the highest number  
+         for (int i = 1; i < candies.length; i++) 
+             if (candies[i] > max) 
+                 max = candies[i];         //Initialize output list
+        List<Boolean> output = new ArrayList<>();         //Loop through each elem to set output[i] to true or false, depending on the sum of candies[i] and extraCandies
+        for(int i =0; i<candies.length; i++){
+            if(candies[i]+extraCandies<max){
+                output.add(false);
+            }else{
+                output.add(true);
+            }
+        }        //finally, return the output list
+        return output;
+    }
+}
