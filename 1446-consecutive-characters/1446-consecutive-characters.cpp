8bab178ed450fe9304545a5c5cef5ab8bcbe2551
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-04-09 18:55:20 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (95.93%) - LeetHub

---
+++ 1446-consecutive-characters/1446-consecutive-characters.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxPower(string s) {
+        int ans = 1, cnt = 1;
+        for (int i = 1; i < s.size(); ++i) {
+            if (s[i] == s[i - 1]) ans = max(ans, ++cnt);
+            else cnt = 1;
+        }
+        return ans;
+    }
+};
