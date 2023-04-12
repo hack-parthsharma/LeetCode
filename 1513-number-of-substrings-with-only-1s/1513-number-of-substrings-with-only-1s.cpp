844f22d9af7350d555e7bef446c0a9c6284a4294
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-04-12 07:51:48 +0530

    Time: 7 ms (94.26%), Space: 8.8 MB (84.26%) - LeetHub

--- 1513-number-of-substrings-with-only-1s/1513-number-of-substrings-with-only-1s.cpp
+++ 1513-number-of-substrings-with-only-1s/1513-number-of-substrings-with-only-1s.cpp
@@ -1,26 +1,17 @@
 class Solution {
-public:
-    // 11 - 1+2
-    // 111 - 1+2+3
-    // 1111 - 1+2+3+4
-    // 11111 - 1+2+3+4+5
-    // so, we just simply find the length of continuous '1',
-    // then, the answer it len*(len+1)/2
-    int numSub(string s) {
-       long long len=0;
-       long long result=0;
-        for (auto c : s) {
-            if (c=='1') {
-                len++;
-                continue;
-            }
-            if (len > 0){
-                result += len*(len+1)/2;
-                len = 0;
-            }
-        }
+ public:
+  int numSub(string s) {
+    constexpr int kMod = 1'000'000'007;
 
-        result += len*(len+1)/2;
-        return result % 1000000007 ;
+    int ans = 0;
+    int l = -1;
+
+    for (int i = 0; i < s.length(); ++i) {
+      if (s[i] == '0')
+        l = i;  // Handle reset value
+      ans = (ans + i - l) % kMod;
     }
+
+    return ans;
+  }
 };
