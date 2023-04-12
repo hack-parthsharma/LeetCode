cd669c13f4ac83389c29e79907e4e36fa5e3b28b
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-04-12 19:05:57 +0530

    Time: 131 ms (27.00%), Space: 63.1 MB (91.98%) - LeetHub

--- 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.cpp
+++ 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.cpp
@@ -1,40 +1,17 @@
 class Solution {
-public:
-    int getWinner(vector<int>& arr, int k) { 
-        
-        int left=0, right=1;
-        
-        int max = arr[left] > arr[right] ? arr[left] : arr[right];
-        int winner; 
-        int win_times = 0;
-        
-        while( right < arr.size())  {
+ public:
+  int getWinner(vector<int>& arr, int k) {
+    int ans = arr[0];
+    int wins = 0;
 
-            //if left < right,  the move the `left` to the `right`
-            if ( arr[left] < arr[right] ) {
-                left = right;
-            }
-            // move the `right` to next element
-            right++;
-            //record current round winner.
-            int w = arr[left];            
-            
-            if (w == winner) {
-                //if winner is same, count++
-                win_times++; 
-            }else{
-                // if winner is new number, reset the count.
-                winner = w;
-                win_times = 1;
-            }
-            
-            // if the time of win equal K, return winner. 
-            if (win_times >= k) return winner;
-            
-            // find the max element of this array, if k > arr.size() then return this
-            if (max < arr[right]) max = arr[right];
-        }
-        
-        return max;
-    }
+    for (int i = 1; i < arr.size() && wins < k; ++i)
+      if (arr[i] > ans) {
+        ans = arr[i];
+        wins = 1;
+      } else {
+        ++wins;
+      }
+
+    return ans;
+  }
 };
