a31d79f0f9552a29966d3c06e465798b193ea8d2
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-04-14 18:30:48 +0530

    Time: 47 ms (86.79%), Space: 8.8 MB (69.81%) - LeetHub

---
+++ 1620-coordinate-with-maximum-network-quality/1620-coordinate-with-maximum-network-quality.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    double dist(int x1, int y1, int x2, int y2){
+        return sqrt((double)((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2)));
+    }
+    
+    vector<int> bestCoordinate(vector<vector<int>>& towers, int radius) {
+        int n = towers.size();
+
+        if(n == 1 && towers[0][2] > 0){
+            return {towers[0][0], towers[0][1]};
+        }
+        
+        int left = INT_MAX, right = INT_MIN;
+        int top = INT_MAX, bottom = INT_MIN;
+        
+        for(const vector<int>& tower : towers){
+            left = min(left, tower[0]);
+            right = max(right, tower[0]);
+            top = min(top, tower[1]);
+            bottom = max(bottom, tower[1]);
+        }
+        
+        double d;
+        int q;
+        int maxq = 0;
+        vector<int> maxcoord(2, 0);
+        for(int x = left; x <= right; ++x){
+            for(int y = top; y <= bottom; ++y){
+                q = 0;
+                for(const vector<int>& tower : towers){
+                    if((d = dist(x, y, tower[0], tower[1])) > radius) continue;
+                    q += tower[2]/(1+d);
+                }
+                if(q > maxq){
+                    maxq = q;
+                    maxcoord[0] = x;
+                    maxcoord[1] = y;
+                }
+            }
+        }
+        
+        return maxcoord;
+    }
+};
