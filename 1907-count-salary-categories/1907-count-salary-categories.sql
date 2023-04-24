038e62cd4393ad66ebd1914768a67c8212b020bb
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-04-24 08:10:35 +0530

    Time: 2601 ms (53.76%), Space: 0B (100.00%) - LeetHub

---
+++ 1907-count-salary-categories/1907-count-salary-categories.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select "Low Salary" as category, count(*) as accounts_count from Accounts where income < 20000
+union 
+select "Average Salary" as category, count(*) as accounts_count from Accounts where 20000 <= income and income <= 50000
+union
+select "High Salary" as category, count(*) as accounts_count from Accounts where 50000 < income
