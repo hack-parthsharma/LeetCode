0f9edfaf37bcf8ff11aa96b75c5e4f33f04fea99
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-06-10 19:30:40 +0530

    Time: 61 ms (34.45%), Space: 41.3 MB (98.33%) - LeetHub

--- 2620-counter/2620-counter.js
+++ 2620-counter/2620-counter.js
@@ -1,16 +1,7 @@
-/**
- * @param {number} n
- * @return {Function} counter
- */
-var createCounter = function(n) {
-    return function() {
-        return n++;
-    };
-};
-
-/** 
- * const counter = createCounter(10)
- * counter() // 10
- * counter() // 11
- * counter() // 12
- */
+function createCounter(n) {
+  let i = -1;
+  return function() {
+    i++;
+    return n + i;
+  };
+}
