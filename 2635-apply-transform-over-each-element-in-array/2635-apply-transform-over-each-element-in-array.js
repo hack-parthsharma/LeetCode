54c1a81ff0a1b82f5006dec5578e112ecc5ed1a6
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-06-12 19:49:59 +0530

    Time: 51 ms (90.44%), Space: 42.2 MB (26.06%) - LeetHub

---
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -0,0 +1,7 @@
+var map = function(arr, fn) {
+    const toReturnArr = new Array(arr.length);
+    for( let i=0;i<arr.length; ++i){
+        toReturnArr[i] = fn(arr[i],i)
+    }
+    return toReturnArr
+};
