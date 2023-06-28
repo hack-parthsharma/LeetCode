a319cb1148f61ada29fd7053dcad3e0bcd0cfc44
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-06-28 07:38:56 +0530

    Time: 11 ms (20.81%), Space: 8.6 MB (14.16%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.cpp
+++ 206-reverse-linked-list/206-reverse-linked-list.cpp
@@ -4,6 +4,7 @@ class Solution {
     if (!head || !head->next)
       return head;
 
+      
     ListNode* newHead = reverseList(head->next);
     head->next->next = head;
     head->next = nullptr;
