033898dbe3dde79d0d59ff5c1987d83ac44c9ee6
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-06-30 07:32:41 +0530

    Time: 3 ms (44.37%), Space: 6 MB (47.15%) - LeetHub

--- 374-guess-number-higher-or-lower/374-guess-number-higher-or-lower.cpp
+++ 374-guess-number-higher-or-lower/374-guess-number-higher-or-lower.cpp
@@ -1,15 +1,3 @@
-/**
- * Forward declaration of guess API.
- * (The problem description is not clear, so I translate it into follows.)
- *
- * @param traget num
- *        guess num
- *
- * @return -1 if guess num >  target num
- *          0 if guess num == target num
- *          1 if guess num <  target num
- */
-
 class Solution {
  public:
   int guessNumber(int n) {
