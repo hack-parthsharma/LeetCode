430256bd2378c8df355cf56999eef64536fc4bad
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-06-30 07:31:51 +0530

    Time: 4 ms (77.55%), Space: 8.8 MB (54.72%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -3,7 +3,6 @@ class Solution {
   int findPeakElement(vector<int>& nums) {
     int l = 0;
     int r = nums.size() - 1;
-
     while (l < r) {
       const int m = (l + r) / 2;
       if (nums[m] >= nums[m + 1])
@@ -11,7 +10,6 @@ class Solution {
       else
         l = m + 1;
     }
-
     return l;
   }
 };
