265b55759aaef2b1cd630069c6c1bd2fbdb6e34a
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-07-01 08:02:31 +0530

    Time: 23 ms (35.44%), Space: 9 MB (40.27%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -3,12 +3,9 @@ class Solution {
   int minDistance(string word1, string word2) {
     const int m = word1.length();
     const int n = word2.length();
-    // dp[i][j] := min # of operations to convert word1[0..i) to word2[0..j)
     vector<vector<int>> dp(m + 1, vector<int>(n + 1));
-
     for (int i = 1; i <= m; ++i)
       dp[i][0] = i;
-
     for (int j = 1; j <= n; ++j)
       dp[0][j] = j;
 
