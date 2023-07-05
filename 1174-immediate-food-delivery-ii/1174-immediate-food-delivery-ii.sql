ad996525fd2481cb455f6089070be6091a0e26f7
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-07-05 10:08:26 +0530

    Time: 1181 ms (83.22%), Space: 0B (100.00%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -2,5 +2,7 @@ select round(sum(case when order_date = customer_pref_delivery_date then 1 else 
 from delivery
 where (customer_id, order_date) in 
     (select customer_id, min(order_date) 
+     
      from delivery 
+     
      group by customer_id)
