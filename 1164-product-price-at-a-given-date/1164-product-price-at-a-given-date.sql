0ef17164c553990c05f6e8ac50a4a1cf90c0f8a2
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-07-07 10:15:56 +0530

    Time: 856 ms (88.74%), Space: 0B (100.00%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,5 +1,7 @@
 select p1.product_id, p1.new_price as price
+
 from Products p1
+
 where p1.change_date <= '2019-08-16' and
     (product_id, datediff('2019-08-16', p1.change_date)) in
     (select product_id, min(datediff('2019-08-16', change_date))
@@ -10,7 +12,11 @@ where p1.change_date <= '2019-08-16' and
 union
 
 select product_id, 10 as price
+
 from Products
+
 group by product_id
+
 having min(change_date) > '2019-08-16'
+
 order by price desc
