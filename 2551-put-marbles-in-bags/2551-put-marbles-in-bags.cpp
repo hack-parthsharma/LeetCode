dc9f670909a9be0d3e79b68f00dac5d02abcd2a6
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-07-08 14:23:01 +0530

    Time: 206 ms (72.85%), Space: 63.5 MB (61.17%) - LeetHub

--- 2551-put-marbles-in-bags/2551-put-marbles-in-bags.cpp
+++ 2551-put-marbles-in-bags/2551-put-marbles-in-bags.cpp
@@ -1,11 +1,7 @@
 class Solution {
  public:
   long long putMarbles(vector<int>& weights, int k) {
-    // To distribute marbles into k bags, there will be k - 1 cuts. If there's a
-    // cut after weights[i], then weights[i] and weights[i + 1] will be added to
-    // the cost. Also, no matter how we cut, weights[0] and weights[n - 1] will
-    // be counted. So, the goal is to find the max/min k - 1 weights[i] +
-    // weights[i + 1].
+
     vector<int> A;  // weights[i] + weights[i + 1]
     long long min = 0;
     long long max = 0;
