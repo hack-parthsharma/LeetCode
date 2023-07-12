5fd4ce7ed5ba5e9d912c0f6c0e12670517149a9a
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-07-12 17:40:57 +0530

    Time: 0 ms (100.00%), Space: 7 MB (92.20%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -10,7 +10,6 @@ class Solution {
   void dfs(const vector<int>& nums, int s, vector<int>&& path,
            vector<vector<int>>& ans) {
     ans.push_back(path);
-
     for (int i = s; i < nums.size(); ++i) {
       path.push_back(nums[i]);
       dfs(nums, i + 1, move(path), ans);
