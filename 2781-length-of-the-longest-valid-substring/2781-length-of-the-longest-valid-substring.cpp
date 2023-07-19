235f19c00cbc4e62e3778d17a2234ac408ca90c6
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-07-19 14:47:52 +0530

    Time: 608 ms (64.95%), Space: 118.2 MB (91.10%) - LeetHub

--- 2781-length-of-the-longest-valid-substring/2781-length-of-the-longest-valid-substring.cpp
+++ 2781-length-of-the-longest-valid-substring/2781-length-of-the-longest-valid-substring.cpp
@@ -3,8 +3,6 @@ class Solution {
   int longestValidSubstring(string word, vector<string>& forbidden) {
     int ans = 0;
     unordered_set<string> forbiddenSet{forbidden.begin(), forbidden.end()};
-
-    // r is the rightmost index to make word[l..r] a valid substring.
     int r = word.length() - 1;
     for (int l = word.length() - 1; l >= 0; --l) {
       for (int end = l; end < min(l + 10, r + 1); ++end)
