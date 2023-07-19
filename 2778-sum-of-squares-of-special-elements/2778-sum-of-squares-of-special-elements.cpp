7e40d13294bd9c52bb636b71e78f5bbfe62ce86d
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-07-19 14:50:34 +0530

    Time: 22 ms (24.32%), Space: 35.4 MB (9.95%) - LeetHub

--- 2778-sum-of-squares-of-special-elements/2778-sum-of-squares-of-special-elements.cpp
+++ 2778-sum-of-squares-of-special-elements/2778-sum-of-squares-of-special-elements.cpp
@@ -3,7 +3,6 @@ class Solution {
   int sumOfSquares(vector<int>& nums) {
     const int n = nums.size();
     int ans = 0;
-
     for (int i = 0; i < n; ++i)
       if (n % (i + 1) == 0)
         ans += nums[i] * nums[i];
