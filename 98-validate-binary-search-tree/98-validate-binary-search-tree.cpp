954f5fb64bd20ffd6a88acacdff2282421c077c8
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-07-20 07:52:53 +0530

    Time: 11 ms (83.37%), Space: 21.6 MB (88.04%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -12,7 +12,6 @@ class Solution {
       return false;
     if (maxNode && root->val >= maxNode->val)
       return false;
-
     return isValidBST(root->left, minNode, root) &&
            isValidBST(root->right, root, maxNode);
   }
