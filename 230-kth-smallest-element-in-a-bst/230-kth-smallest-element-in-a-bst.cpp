c1df13662c79274e1020659694958651a64d458b
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-07-21 07:44:50 +0530

    Time: 11 ms (98.56%), Space: 24 MB (67.85%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
@@ -2,14 +2,12 @@ class Solution {
  public:
   int kthSmallest(TreeNode* root, int k) {
     const int leftCount = countNodes(root->left);
-
     if (leftCount == k - 1)
       return root->val;
     if (leftCount >= k)
       return kthSmallest(root->left, k);
     return kthSmallest(root->right, k - 1 - leftCount);  
   }
-
  private:
   int countNodes(TreeNode* root) {
     if (root == nullptr)
