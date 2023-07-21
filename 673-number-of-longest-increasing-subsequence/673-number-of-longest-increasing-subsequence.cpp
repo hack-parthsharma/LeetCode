1bb888b0a3b6fa969382f46a6a3b89604f5a91e9
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-07-21 07:39:30 +0530

    Time: 157 ms (85.06%), Space: 13.3 MB (25.03%) - LeetHub

--- 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.cpp
+++ 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.cpp
@@ -6,7 +6,6 @@ class Solution {
     int maxLength = 0;
     vector<int> length(n, 1);  
     vector<int> count(n, 1);   
-
     for (int i = 0; i < n; ++i)
       for (int j = 0; j < i; ++j)
         if (nums[j] < nums[i])
