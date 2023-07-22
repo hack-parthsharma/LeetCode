d69327ab921c971c34f79bfb2ce3ec1fe89aa810
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-07-22 13:23:36 +0530

    Time: 3 ms (86.63%), Space: 7.4 MB (18.64%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -7,7 +7,6 @@ class Solution {
     for (int i = 1; i < s2.length(); ++i)
       if (s2[i] == ')' && s2[i - dp[i - 1] - 1] == '(')
         dp[i] = dp[i - 1] + dp[i - dp[i - 1] - 2] + 2;
-
     return *max_element(begin(dp), end(dp));
   }
 };
