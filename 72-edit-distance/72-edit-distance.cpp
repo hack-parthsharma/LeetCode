724fb3bf2a5b13a4f6cc3e65368c4ef07bef0bd9
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-07-23 11:48:34 +0530

    Time: 24 ms (32.78%), Space: 9 MB (60.99%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -15,7 +15,6 @@ class Solution {
           dp[i][j] = dp[i - 1][j - 1];
         else
           dp[i][j] = min({dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]}) + 1;
-
     return dp[m][n];
   }
 };
