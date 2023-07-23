42e5465fb06be5bb99c8c40d02857f84e3cb1c52
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-07-23 11:49:18 +0530

    Time: 5 ms (35.04%), Space: 6.6 MB (72.88%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -5,7 +5,6 @@ class Solution {
 
     for (int i = 0; i < numRows; ++i)
       ans.push_back(vector<int>(i + 1, 1));
-
     for (int i = 2; i < numRows; ++i)
       for (int j = 1; j < ans[i].size() - 1; ++j)
         ans[i][j] = ans[i - 1][j - 1] + ans[i - 1][j];
