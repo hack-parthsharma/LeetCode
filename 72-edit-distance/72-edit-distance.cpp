25572ba9e938448b73b76bfc67811dad5f70c311
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-07-23 11:48:44 +0530

    Time: 21 ms (39.30%), Space: 9 MB (39.00%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -8,7 +8,6 @@ class Solution {
       dp[i][0] = i;
     for (int j = 1; j <= n; ++j)
       dp[0][j] = j;
-
     for (int i = 1; i <= m; ++i)
       for (int j = 1; j <= n; ++j)
         if (word1[i - 1] == word2[j - 1])
