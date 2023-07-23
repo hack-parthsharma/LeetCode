ae67139bf1af98c7bc342f87680490336696f41a
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-07-23 11:49:08 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (95.34%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -9,7 +9,6 @@ class Solution {
     for (int i = 2; i < numRows; ++i)
       for (int j = 1; j < ans[i].size() - 1; ++j)
         ans[i][j] = ans[i - 1][j - 1] + ans[i - 1][j];
-
     return ans;
   }
 };
