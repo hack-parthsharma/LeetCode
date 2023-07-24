157970091e16ac3f6a106ebb14a41aa8ddadf770
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-07-24 19:35:44 +0530

    Time: 53 ms (100.00%), Space: 8 MB (14.29%) - LeetHub

--- 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -4,9 +4,7 @@ class Solution {
     constexpr int kMod = 1'000'000'007;
     vector<int> dp(n + 1);
     int ax; 
-
     dp[0] = 1;
-
     for (int a = 1; (ax = pow(a, x)) <= n; ++a)
       for (int i = n; i >= ax; --i) {
         dp[i] += dp[i - ax];
