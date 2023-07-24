825f569625ef2709618356ff8e6030c0e54330ec
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-07-24 19:37:39 +0530

    Time: 223 ms (100.00%), Space: 114 MB (100.00%) - LeetHub

--- 2790-maximum-number-of-groups-with-increasing-length/2790-maximum-number-of-groups-with-increasing-length.cpp
+++ 2790-maximum-number-of-groups-with-increasing-length/2790-maximum-number-of-groups-with-increasing-length.cpp
@@ -3,15 +3,12 @@ class Solution {
   int maxIncreasingGroups(vector<int>& usageLimits) {
     int ans = 1; 
     long long availableLimits = 0;
-
     sort(usageLimits.begin(), usageLimits.end());
-
     for (const int usageLimit : usageLimits) {
       availableLimits += usageLimit;
       if (availableLimits >= ans * static_cast<long>(ans + 1) / 2)
         ++ans;
     }
-
     return ans - 1;
   }
 };
