e8185f647429eb2afac026d472be21ef71ea3a74
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-07-24 19:34:50 +0530

    Time: 198 ms (100.00%), Space: 99.5 MB (93.33%) - LeetHub

--- 2786-visit-array-positions-to-maximize-score/2786-visit-array-positions-to-maximize-score.cpp
+++ 2786-visit-array-positions-to-maximize-score/2786-visit-array-positions-to-maximize-score.cpp
@@ -1,12 +1,7 @@
 class Solution {
  public:
   long long maxScore(vector<int>& nums, int x) {
-    // Note that we always need to take nums[0], so the initial definition might
-    // not hold true.
-
-    // dp0 := max score so far with nums ending in an even number
     long long dp0 = nums[0] - (nums[0] % 2 == 1 ? x : 0);
-    // dp1 := max score so far with nums ending in an odd number
     long long dp1 = nums[0] - (nums[0] % 2 == 0 ? x : 0);
 
     for (int i = 1; i < nums.size(); ++i)
