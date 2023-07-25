0aeadc383ea52b96ad66e32369293f80e50b9cd8
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-07-25 15:32:18 +0530

    Time: 200 ms (71.86%), Space: 9.2 MB (56.83%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -4,7 +4,7 @@ class Solution {
     vector<int> dp(n + 1, n);  
 
     dp[0] = 0;  
-    dp[1] = 1;  // 1^2
+    dp[1] = 1;  
 
     for (int i = 2; i <= n; ++i)
       for (int j = 1; j * j <= i; ++j)
