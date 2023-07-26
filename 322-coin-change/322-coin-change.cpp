0d7a508e0b25d9592474828525d8687365062492
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-07-26 14:52:57 +0530

    Time: 90 ms (81.99%), Space: 14.4 MB (70.13%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -3,7 +3,6 @@ class Solution {
   int coinChange(vector<int>& coins, int amount) {
     vector<int> dp(amount + 1, amount + 1);
     dp[0] = 0;
-
     for (const int coin : coins)
       for (int i = coin; i <= amount; ++i)
         dp[i] = min(dp[i], dp[i - coin] + 1);
