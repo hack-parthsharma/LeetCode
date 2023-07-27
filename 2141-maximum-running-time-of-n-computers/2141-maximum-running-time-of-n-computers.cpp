0663ac2b50f4331dffa6ec099e10a13131065b06
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-07-27 07:40:55 +0530

    Time: 125 ms (99.64%), Space: 55.8 MB (71.58%) - LeetHub

--- 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
@@ -2,7 +2,6 @@ class Solution {
  public:
   long long maxRunTime(int n, vector<int>& batteries) {
     long long sum = accumulate(begin(batteries), end(batteries), 0LL);
-
     sort(begin(batteries), end(batteries));
     while (batteries.back() > sum / n) {
       sum -= batteries.back(), batteries.pop_back();
