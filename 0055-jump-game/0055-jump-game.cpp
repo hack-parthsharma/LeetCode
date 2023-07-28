35ffb2c711351235777e2fdcb8db9909ebfb49a6
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-07-28 07:45:28 +0530

    Time: 46 ms (99.83%), Space: 48.4 MB (40.38%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -2,7 +2,6 @@ class Solution {
  public:
   bool canJump(vector<int>& nums) {
     int i = 0;
-
     for (int reach = 0; i < nums.size() && i <= reach; ++i)
       reach = max(reach, i + nums[i]);
     return i == nums.size();
