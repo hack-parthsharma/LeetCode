cc3f0fa98b67f42cfd61498a88e8c9b6adf89fca
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-07-28 07:47:50 +0530

    Time: 7 ms (97.09%), Space: 10.9 MB (31.34%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,7 +2,6 @@ class Solution {
  public:
   vector<int> twoSum(vector<int>& nums, int target) {
     unordered_map<int, int> numToIndex;
-
     for (int i = 0; i < nums.size(); ++i) {
       if (const auto it = numToIndex.find(target - nums[i]);
           it != cend(numToIndex))
