972fd8190586ebcef1e3cfed9606a105190a16cf
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-07-31 17:01:04 +0530

    Time: 39 ms (78.39%), Space: 15.2 MB (41.16%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -7,7 +7,6 @@ public:
         // s1 is row, s2 is column
         for (int i=1; i<=s2.size(); i++) dp[0][i] = dp[0][i-1] + s2[i-1];
         for (int i=1; i<=s1.size(); i++) dp[i][0] = dp[i-1][0] + s1[i-1];
-
         for (int i=1; i<=s1.size(); i++){
             for (int j=1; j<=s2.size(); j++) {
                 if ( s1[i-1] == s2[j-1] ) {
@@ -17,7 +16,6 @@ public:
                 }
             }
         }
-
         return dp[s1.size()][s2.size()];
     }
 };
