b4be79c10bad10516cc99d2bbba029d669ed4ddf
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-08-04 07:52:38 +0530

    Time: 2473 ms (62.93%), Space: 0B (100.00%) - LeetHub

--- 1907-count-salary-categories/1907-count-salary-categories.sql
+++ 1907-count-salary-categories/1907-count-salary-categories.sql
@@ -1,5 +1,6 @@
 select "Low Salary" as category, count(*) as accounts_count from Accounts where income < 20000
 union 
+
 select "Average Salary" as category, count(*) as accounts_count from Accounts where 20000 <= income and income <= 50000
 union
 select "High Salary" as category, count(*) as accounts_count from Accounts where 50000 < income
