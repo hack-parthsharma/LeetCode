01eb3ddd64c9a58182cbf6fc96ce191d047ec755
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-08-04 07:54:04 +0530

    Time: 693 ms (99.28%), Space: 0B (100.00%) - LeetHub

---
+++ 1173-immediate-food-delivery-i/1173-immediate-food-delivery-i.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+SELECT
+    round(
+        sum(order_date = customer_pref_delivery_date) * 100 / count(1),
+        2
+    ) AS immediate_percentage
+FROM Delivery;
