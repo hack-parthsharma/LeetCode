7df8fdd3048f1d2db02ba9e18a14f40b21d6ff4a
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-08-04 07:51:13 +0530

    Time: 990 ms (19.57%), Space: 0B (100.00%) - LeetHub

---
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -0,0 +1,13 @@
+create function getNthHighestSalary(N int) returns int
+begin
+  return (
+      
+      select distinct salary as 'getNthHighestSalary(2)'
+       from  (
+          select salary,
+                dense_rank() over(order by salary desc) as rnk
+          from employee
+      )  sal_rnk
+      where rnk = n      
+  );
+end
