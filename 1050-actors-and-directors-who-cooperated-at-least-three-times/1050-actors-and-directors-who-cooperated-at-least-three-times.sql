ac9ae0c9a89491fef0c506286e4eed21d80f5e0d
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-08-05 07:58:30 +0530

    Time: 648 ms (90.80%), Space: 0B (100.00%) - LeetHub

--- 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
+++ 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -1,9 +1,3 @@
-# select actor_id, director_id
-# from (
-#   select actor_id, director_id, count(*) as cnt
-#   from ActorDirector
-#   group by actor_id, director_id
-#   having cnt >= 3) as e;
 
 select actor_id, director_id
 from ActorDirector
