3ab5827467768eab2952c7316379b2edfa4a284c
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-08-06 10:32:41 +0530

    Time: 11 ms (21.17%), Space: 9.3 MB (10.81%) - LeetHub

--- 0920-number-of-music-playlists/0920-number-of-music-playlists.cpp
+++ 0920-number-of-music-playlists/0920-number-of-music-playlists.cpp
@@ -4,7 +4,6 @@ class Solution {
     constexpr int kMod = 1'000'000'007;
     vector<vector<long>> dp(goal + 1, vector<long>(n + 1));
     dp[0][0] = 1;
-
     for (int i = 1; i <= goal; ++i)
       for (int j = 1; j <= n; ++j) {
         dp[i][j] += dp[i - 1][j - 1] * (n - (j - 1));  
