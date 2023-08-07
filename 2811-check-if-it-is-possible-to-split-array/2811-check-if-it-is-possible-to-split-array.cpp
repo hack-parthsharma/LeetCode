1e4d086885991c20627798dfe1dc5ae70522d32a
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-08-07 13:53:08 +0530

    Time: 13 ms (66.67%), Space: 17.6 MB (66.67%) - LeetHub

--- 2811-check-if-it-is-possible-to-split-array/2811-check-if-it-is-possible-to-split-array.cpp
+++ 2811-check-if-it-is-possible-to-split-array/2811-check-if-it-is-possible-to-split-array.cpp
@@ -3,7 +3,6 @@ class Solution {
   bool canSplitArray(vector<int>& nums, int m) {
     if (nums.size() < 3)
       return true;
-
     for (int i = 1; i < nums.size(); ++i)
       if (nums[i] + nums[i - 1] >= m)
         return true;
