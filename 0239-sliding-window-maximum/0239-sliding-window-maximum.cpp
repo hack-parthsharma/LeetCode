5ebf931d413ec8a00200a047dc9594af0124c253
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-08-11 07:49:08 +0530

    Time: 180 ms (99.29%), Space: 134.7 MB (45.79%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -2,7 +2,7 @@ class Solution {
  public:
   vector<int> maxSlidingWindow(vector<int>& nums, int k) {
     vector<int> ans;
-    deque<int> q;  // Max queue
+    deque<int> q;  
 
     for (int i = 0; i < nums.size(); ++i) {
       while (!q.empty() && q.back() < nums[i])
