d69afdb17b9d18c124bc62045e8a3dc99f75e0f3
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-08-12 10:33:40 +0530

    Time: 2 ms (68.17%), Space: 7.8 MB (25.52%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -5,7 +5,6 @@ class Solution {
     const int n = obstacleGrid[0].size();
     vector<vector<long>> dp(m + 1, vector<long>(n + 1, 0));
     dp[0][1] = 1;  
-
     for (int i = 1; i <= m; ++i)
       for (int j = 1; j <= n; ++j)
         if (!obstacleGrid[i - 1][j - 1])
