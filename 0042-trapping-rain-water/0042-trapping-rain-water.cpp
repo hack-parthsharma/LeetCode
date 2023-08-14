d8d60ced60935bdc205630fa38a276cac12ca0d3
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-08-14 13:52:17 +0530

    Time: 15 ms (66.02%), Space: 20.5 MB (17.53%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -9,7 +9,6 @@ class Solution {
       l[i] = i == 0 ? height[i] : max(height[i], l[i - 1]);
     for (int i = n - 1; i >= 0; --i)
       r[i] = i == n - 1 ? height[i] : max(height[i], r[i + 1]);
-
     for (int i = 0; i < n; ++i)
       ans += min(l[i], r[i]) - height[i];
     return ans;
