cf33b908ab35d9642d7b76d86f6142779f039501
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-08-18 07:42:28 +0530

    Time: 0 ms (100.00%), Space: 8.9 MB (81.84%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -2,7 +2,7 @@ class Solution {
  public:
   void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
     int i = m - 1;      // nums1's index (actual nums)
-    int j = n - 1;      // nums2's index
+    int j = n - 1;      
     int k = m + n - 1;  
     while (j >= 0)
       if (i >= 0 && nums1[i] > nums2[j])
