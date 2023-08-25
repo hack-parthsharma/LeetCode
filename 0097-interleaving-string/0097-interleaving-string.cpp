04024e365060a93b8f7a81376c2752237a631c21
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-08-25 08:09:01 +0530

    Time: 3 ms (78.62%), Space: 6.7 MB (71.67%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -11,7 +11,6 @@ class Solution {
       dp[i][0] = dp[i - 1][0] && s1[i - 1] == s3[i - 1];
     for (int j = 1; j <= n; ++j)
       dp[0][j] = dp[0][j - 1] && s2[j - 1] == s3[j - 1];
-
     for (int i = 1; i <= m; ++i)
       for (int j = 1; j <= n; ++j)
         dp[i][j] = dp[i - 1][j] && s1[i - 1] == s3[i + j - 1] ||
