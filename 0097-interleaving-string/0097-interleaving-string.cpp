71d9abab3c99cb8530dbd30ad8fcd969fdd21918
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-08-25 08:08:22 +0530

    Time: 6 ms (55.25%), Space: 6.8 MB (66.93%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -7,10 +7,8 @@ class Solution {
       return false;
     vector<vector<bool>> dp(m + 1, vector<bool>(n + 1));
     dp[0][0] = true;
-
     for (int i = 1; i <= m; ++i)
       dp[i][0] = dp[i - 1][0] && s1[i - 1] == s3[i - 1];
-
     for (int j = 1; j <= n; ++j)
       dp[0][j] = dp[0][j - 1] && s2[j - 1] == s3[j - 1];
 
