49d170d25427388d13da45232b9e6238f7924370
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-08-30 09:47:56 +0530

    Time: 96 ms (60.96%), Space: 54.7 MB (42.04%) - LeetHub

--- 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.cpp
+++ 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.cpp
@@ -2,7 +2,6 @@ class Solution {
  public:
   long long minimumReplacement(vector<int>& nums) {
     long long ans = 0;
-
     int max = nums.back();
     for (int i = nums.size() - 2; i >= 0; --i) {
       const int ops = (nums[i] - 1) / max;
