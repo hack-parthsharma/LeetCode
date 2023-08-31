1dd46e927dd5ec08f1af1e12e3ca2a0d1c4ee142
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-08-31 08:15:19 +0530

    Time: 3 ms (41.41%), Space: 6.5 MB (8.69%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -4,7 +4,6 @@ public:
         int ans=-1;
         int niddilesize=needle.size();
         int k=0;     
-
         while(k<haystack.size()){
 
         auto itr=find(haystack.begin()+k,haystack.end(),needle[0]);
