c9a842481960cac139f056e56d5c224cba607f9d
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-09-10 10:54:50 +0530

    Time: 2 ms (56.67%), Space: 6 MB (71.30%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -3,7 +3,6 @@ class Solution {
   int countOrders(int n) {
     constexpr int kMod = 1'000'000'007;
     long ans = 1;
-
     for (int i = 1; i <= n; ++i)
       ans = ans * i * (i * 2 - 1) % kMod;
 
