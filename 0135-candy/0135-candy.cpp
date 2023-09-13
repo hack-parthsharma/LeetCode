628db4bab4b60e82071aa817b66946ea3482d960
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-09-13 14:39:49 +0530

    Time: 19 ms (45.37%), Space: 18.2 MB (24.67%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -9,7 +9,8 @@ class Solution {
      for (int i = 1; i < n; ++i)
       if (ratings[i] > ratings[i - 1])
         l[i] = l[i - 1] + 1;
-    for (int i = n - 2; i >= 0; --i)
+    
+     for (int i = n - 2; i >= 0; --i)
       if (ratings[i] > ratings[i + 1])
         r[i] = r[i + 1] + 1;
     for (int i = 0; i < n; ++i)
