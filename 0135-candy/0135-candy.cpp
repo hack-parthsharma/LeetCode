7b8962fa1edcc5c44a8a329705e1dcddc363da01
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-09-13 14:39:37 +0530

    Time: 17 ms (62.30%), Space: 18.2 MB (24.67%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -5,7 +5,8 @@ class Solution {
     int ans = 0;
     vector<int> l(n, 1);
     vector<int> r(n, 1);
-    for (int i = 1; i < n; ++i)
+    
+     for (int i = 1; i < n; ++i)
       if (ratings[i] > ratings[i - 1])
         l[i] = l[i - 1] + 1;
     for (int i = n - 2; i >= 0; --i)
