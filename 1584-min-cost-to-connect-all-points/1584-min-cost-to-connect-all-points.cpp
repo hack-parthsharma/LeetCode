dde5d5809e0203e4b0a71ad20d68d16b378c0b46
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-09-15 08:08:30 +0530

    Time: 142 ms (83.96%), Space: 58.4 MB (56.50%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -30,6 +30,7 @@ public:
     int minCostConnectPoints(vector<vector<int>>& A) {
         int N = A.size(), ans = 0;
         vector<array<int, 3>> E;
+        
         for (int i = 0; i < N; ++i) {
             for (int j = i + 1; j < N; ++j) E.push_back({ abs(A[i][0] - A[j][0]) + abs(A[i][1] - A[j][1]), i, j });
         }
