0a5b44eec5f4a4cb24b95cff6eca9d1186909ebc
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-09-22 07:18:38 +0530

    Time: 3 ms (49.10%), Space: 6.6 MB (17.60%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -5,7 +5,8 @@ bool isSubsequence(string s, string t) {
     
 if (s.empty())
       return true;
-    int i = 0;
+    
+int i = 0;
     for (const char c : t)
       if (s[i] == c && ++i == s.length())
         return true;
