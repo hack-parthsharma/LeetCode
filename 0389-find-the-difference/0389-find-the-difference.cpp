1427b38321e6b886885d27ba9fc3720c66b8bcc8
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-09-25 16:34:15 +0530

    Time: 4 ms (48.11%), Space: 7 MB (16.46%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,13 @@
+class Solution {
+ public:
+  char findTheDifference(string s, string t) {
+    char ans = 0;
+    for (const char c : s)
+      ans ^= c;
+
+    for (const char c : t)
+      ans ^= c;
+
+    return ans;
+  }
+};
