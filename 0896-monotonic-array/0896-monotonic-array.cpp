37c97d66780718a85f2a16c08c76a118ed2b2587
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-09-29 07:50:17 +0530

    Time: 115 ms (82.11%), Space: 96.8 MB (22.05%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.cpp
+++ 0896-monotonic-array/0896-monotonic-array.cpp
@@ -3,7 +3,7 @@ class Solution {
   
  bool isMonotonic(vector<int>& nums) {
  bool increasing = true;
-    bool decreasing = true;
+ bool decreasing = true;
     for (int i = 1; i < nums.size(); ++i) {
       increasing &= nums[i] >= nums[i - 1];
       decreasing &= nums[i] <= nums[i - 1];
