3bf7574d4e9fc7333cc0387ac11ceace7d07674d
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-09-30 17:34:03 +0530

    Time: 69 ms (52.94%), Space: 48.9 MB (32.15%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.cpp
+++ 0456-132-pattern/0456-132-pattern.cpp
@@ -2,7 +2,7 @@ class Solution {
  public:
   bool find132pattern(vector<int>& nums) {
     stack<int> stack;  
-    int ak = INT_MIN;  // We want to find a seq ai < ak < aj
+    int ak = INT_MIN;  
 
     for (int i = nums.size() - 1; i >= 0; --i) {
       // Ai < ak, we're done because ai must also smaller than aj
