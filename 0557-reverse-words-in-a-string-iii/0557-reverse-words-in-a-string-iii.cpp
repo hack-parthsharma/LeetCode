dcbb30e86c66fb579ce1ee0125ede3caa9d36ca8
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-10-01 17:49:18 +0530

    Time: 6 ms (99.05%), Space: 10.1 MB (52.15%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -6,7 +6,8 @@ class Solution {
     int j = 0;
     
     while (i < s.length()) {
-      while (i < j || i < s.length() && s[i] == ' ')
+      
+    while (i < j || i < s.length() && s[i] == ' ')
         ++i;
       while (j < i || j < s.length() && s[j] != ' ')
         ++j;
