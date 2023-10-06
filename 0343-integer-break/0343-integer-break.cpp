6d5f58225eafb4ad85866bbdc5286e25e0920e9a
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-10-06 11:32:13 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (53.64%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,7 +1,6 @@
 class Solution {
  public:
   int integerBreak(int n) {
-    // (and 1 is wasteful).
     // Also, 3 * 3 is better than 2 * 2 * 2, so we never use 2 more than twice.
     if (n == 2)  // 1 * 1
       return 1;
