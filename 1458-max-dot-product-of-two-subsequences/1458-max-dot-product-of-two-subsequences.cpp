75de186f78bc346583345e966bbaf708656d3196
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-10-08 16:25:27 +0530

    Time: 23 ms (80.98%), Space: 13.6 MB (22.88%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
@@ -4,7 +4,6 @@ class Solution {
     const int m = nums1.size();
     const int n = nums2.size();
     vector<vector<int>> dp(m + 1, vector<int>(n + 1, INT_MIN));
-
     for (int i = 0; i < m; ++i)
       for (int j = 0; j < n; ++j)
         dp[i + 1][j + 1] = max({dp[i][j + 1], dp[i + 1][j],
