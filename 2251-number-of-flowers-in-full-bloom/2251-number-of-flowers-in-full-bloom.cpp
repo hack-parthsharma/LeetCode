f0dbe4abe6ab93ad0404735d7d5e21c7ad997a15
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-10-11 12:31:13 +0530

    Time: 232 ms (82.03%), Space: 82.4 MB (80.40%) - LeetHub

--- 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.cpp
+++ 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.cpp
@@ -13,7 +13,7 @@ for (const vector<int>& f : flowers) {
 for (const int p : persons) {
       const int started =
           upper_bound(begin(starts), end(starts), p) - begin(starts);
-      const int ended = lower_bound(begin(ends), end(ends), p) - begin(ends);
+const int ended = lower_bound(begin(ends), end(ends), p) - begin(ends);
       ans.push_back(started - ended);
     }
 return ans;
