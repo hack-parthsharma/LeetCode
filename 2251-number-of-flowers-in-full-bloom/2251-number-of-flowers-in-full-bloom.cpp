198b00aa9276a7522a484227c7bcd371c606f672
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-10-11 12:30:13 +0530

    Time: 224 ms (90.74%), Space: 82.4 MB (80.40%) - LeetHub

--- 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.cpp
+++ 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.cpp
@@ -1,7 +1,6 @@
 class Solution {
  public:
-vector<int> fullBloomFlowers(vector<vector<int>>& flowers,
-                               vector<int>& persons) {
+vector<int> fullBloomFlowers(vector<vector<int>>& flowers, vector<int>& persons) {
 vector<int> ans;
 vector<int> starts;
 vector<int> ends;
