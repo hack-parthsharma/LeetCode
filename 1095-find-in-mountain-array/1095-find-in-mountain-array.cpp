6f779df3c2d8e68e2112b6d334b0ad039f19fd15
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-10-12 19:16:28 +0530

    Time: 3 ms (78.78%), Space: 7.3 MB (43.62%) - LeetHub

--- 1095-find-in-mountain-array/1095-find-in-mountain-array.cpp
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.cpp
@@ -1,12 +1,4 @@
-/**
- * // This is the MountainArray's API interface.
- * // You should not implement it, or speculate about its implementation
- * class MountainArray {
- *  public:
- *   int get(int index);
- *   int length();
- * };
- */
+
 
 class Solution {
  public:
