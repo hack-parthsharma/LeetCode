9bf311d6ecf4c56bdd4b66e86d573bbfd2eb4082
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-10-14 16:07:56 +0530

    Time: 205 ms (23.56%), Space: 91.6 MB (65.68%) - LeetHub

--- 2742-painting-the-walls/2742-painting-the-walls.cpp
+++ 2742-painting-the-walls/2742-painting-the-walls.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 int paintWalls(vector<int>& cost, vector<int>& time) {
-        int n = cost.size();
+int n = cost.size();
         int f[n][n << 1 | 1];
         memset(f, -1, sizeof(f));
         function<int(int, int)> dfs = [&](int i, int j) -> int {
