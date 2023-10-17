01250dc1050a42d163e0cac9832be1cda835abd4
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-10-17 19:03:43 +0530

    Time: 40 ms (57.45%), Space: 32.7 MB (73.25%) - LeetHub

--- 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.cpp
+++ 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.cpp
@@ -1,7 +1,6 @@
 class Solution {
  public:
-bool validateBinaryTreeNodes(int n, vector<int>& leftChild,
-                               vector<int>& rightChild) {
+bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild) {
     vector<int> inDegree(n);
     int root = -1;
 
