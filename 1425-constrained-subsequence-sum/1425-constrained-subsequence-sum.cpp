755afde6d59b86e4452bc7b318aab61f951754bd
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-10-21 06:24:10 +0530

    Time: 196 ms (89.39%), Space: 122.5 MB (59.80%) - LeetHub

--- 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.cpp
+++ 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.cpp
@@ -2,9 +2,7 @@ class Solution {
  public:
   int constrainedSubsetSum(vector<int>& nums, int k) {
     vector<int> dp(nums.size());
-    // Decreasing order
     deque<int> q;
-
     for (int i = 0; i < nums.size(); ++i) {
       if (q.empty())
         dp[i] = nums[i];
