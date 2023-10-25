a7975388c90d17b3ac5d0bf49b5b73426953d4e1
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-10-25 16:57:40 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (28.86%) - LeetHub

--- 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
 int kthGrammar(int n, int k) {
 if (n == 1)
-      return 0;
+return 0;
 if (k & 1)
       return kthGrammar(n - 1, (k + 1) / 2) != 0;  
       return kthGrammar(n - 1, k / 2) == 0;         
