2cc755a0fab7d979d00996f95eead1798f486711
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-10-28 14:59:19 +0530

    Time: 231 ms (56.25%), Space: 16.5 MB (68.47%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.py
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.py
@@ -2,7 +2,6 @@ class Solution:
   def countVowelPermutation(self, n: int) -> int:
     kMod = 1_000_000_007
     dp = {'a': 1, 'e': 1, 'i': 1, 'o': 1, 'u': 1}
-
     for _ in range(n - 1):
       newDp = {'a': dp['e'] + dp['i'] + dp['u'],
                'e': dp['a'] + dp['i'],
@@ -10,5 +9,4 @@ class Solution:
                'o': dp['i'],
                'u': dp['i'] + dp['o']}
       dp = newDp
-
     return sum(dp.values()) % kMod
