2fadde0c1cbbe00aafcc02db2694b0cf9c0a2108
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-10-29 11:43:51 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (39.09%) - LeetHub

--- 0458-poor-pigs/0458-poor-pigs.cpp
+++ 0458-poor-pigs/0458-poor-pigs.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
-    int times = minutesToTest / minutesToDie;
+int times = minutesToTest / minutesToDie;
     int result = 0;
     while (pow(times + 1, result) < buckets) ++result;    return result;
   }
