3f2108954327dcb8e5bc583f9a4abf7f93a32c27
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-11-08 07:47:08 +0530

    Time: 2 ms (36.53%), Space: 6.5 MB (5.30%) - LeetHub

--- 2849-determine-if-a-cell-is-reachable-at-a-given-time/2849-determine-if-a-cell-is-reachable-at-a-given-time.cpp
+++ 2849-determine-if-a-cell-is-reachable-at-a-given-time/2849-determine-if-a-cell-is-reachable-at-a-given-time.cpp
@@ -1,5 +1,5 @@
 class Solution {
- public:
+public:
   bool isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
     const int minStep = max(abs(sx - fx), abs(sy - fy));
     return minStep == 0 ? t != 1 : minStep <= t;
