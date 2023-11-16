afbabbb7e35912e9e1ff34d0a022878d40047cd8
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-11-16 18:16:13 +0530

    Time: 3 ms (74.62%), Space: 10.7 MB (52.42%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -6,11 +6,9 @@ class Solution {
     unordered_set<int> numsSet;
     for (const string& num : nums)
       numsSet.insert(stoi(num, nullptr, 2));
-
     for (int num = 0; num < maxNum; ++num)
       if (!numsSet.count(num))
         return std::bitset<16>(num).to_string().substr(16 - bitSize);
-
     throw;
   }
 };
