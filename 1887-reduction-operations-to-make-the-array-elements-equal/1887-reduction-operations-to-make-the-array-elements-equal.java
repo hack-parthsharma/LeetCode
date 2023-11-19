154ae45c1571bbbea5453a6902ac8492183fc6b1
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-11-19 08:37:29 +0530

    Time: 33 ms (92.11%), Space: 55.2 MB (25.66%) - LeetHub

--- 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.java
+++ 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.java
@@ -1,8 +1,8 @@
 class Solution {
-    public int reductionOperations(int[] nums) {
+public int reductionOperations(int[] nums) {
         Arrays.sort(nums);
         int ans = 0, cnt = 0;
-        for (int i = 1; i < nums.length; ++i) {
+for (int i = 1; i < nums.length; ++i) {
             if (nums[i] != nums[i - 1]) {
                 ++cnt;
             }
