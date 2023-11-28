0731f8f95ae54f2bbe0f54f58209d3f9bbb62ab0
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-11-28 11:19:31 +0530

    Time: 111 ms (92.00%), Space: 31 MB (84.57%) - LeetHub

--- 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
@@ -5,7 +5,8 @@ public:
     long ans = 1;
     int prevSeat = -1;
     int numSeats = 0;
-    for (int i = 0; i < corridor.length(); ++i) {
+    
+for (int i = 0; i < corridor.length(); ++i) {
       if (corridor[i] == 'S') {
         if (++numSeats > 2 && numSeats & 1)
           ans = ans * (i - prevSeat) % kMod;
