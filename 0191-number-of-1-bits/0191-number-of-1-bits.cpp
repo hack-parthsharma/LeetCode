c2e33c22960b75cca14335581f3e8b40fa0c08eb
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-11-29 07:36:16 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (9.00%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -2,7 +2,8 @@ class Solution {
  public:
   int hammingWeight(uint32_t n) {
     int ans = 0;
-    for (int i = 0; i < 32; ++i)
+
+for (int i = 0; i < 32; ++i)
       if ((n >> i) & 1)
         ++ans;
     return ans;
