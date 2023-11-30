dfc23998937457525dc54b838de6486daf2c436e
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-11-30 07:36:47 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (80.09%) - LeetHub

--- 1611-minimum-one-bit-operations-to-make-integers-zero/1611-minimum-one-bit-operations-to-make-integers-zero.cpp
+++ 1611-minimum-one-bit-operations-to-make-integers-zero/1611-minimum-one-bit-operations-to-make-integers-zero.cpp
@@ -5,8 +5,10 @@ public:
 int minimumOneBitOperations(int n) {
         
 if (n <= 1) return n;
-        unsigned first = ~0;
-        while (first & n) first <<= 1;
+        
+unsigned first = ~0;
+        
+while (first & n) first <<= 1;
         first = lowbit(first >> 1);
         unsigned second = (first >> 1) & n, rest = minimumOneBitOperations(n & ~first & ~second);
         return second ? first + rest : (first << 1) - 1 - rest;
