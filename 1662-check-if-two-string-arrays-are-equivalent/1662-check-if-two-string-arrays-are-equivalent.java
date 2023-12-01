28e1309b4c5f6146bf8801fa2b39317da975124f
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-12-01 07:33:41 +0530

    Time: 1 ms (73.30%), Space: 40.8 MB (22.03%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
@@ -14,7 +14,8 @@ public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
             w2+=word2[i];
         }
         String ans2=w2;
-        if(w1.equals(w2))
+        
+    if(w1.equals(w2))
         {
             return true;
         }
