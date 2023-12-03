5ef05a46a7672e6ccfbe18537ef45198992dbb31
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-12-03 13:46:45 +0530

    Time: 10 ms (33.18%), Space: 10.4 MB (95.02%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
@@ -5,7 +5,6 @@ class Solution {
     for (int i = 1; i < points.size(); ++i)
       ans += max(abs(points[i][0] - points[i - 1][0]),
                  abs(points[i][1] - points[i - 1][1]));
-
     return ans;
   }
 };
