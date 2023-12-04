5b7d6a21ae68b7f780e5069f6e7bc878d449083d
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-12-04 18:37:21 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (31.59%) - LeetHub

--- 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
@@ -6,6 +6,6 @@ for (int i = 2; i < num.length(); ++i)
       if (num[i - 2] == num[i - 1] && num[i - 1] == num[i])
         ans = max(ans, num.substr(i - 2, 3));
 
-    return ans;
+return ans;
 }
 };
