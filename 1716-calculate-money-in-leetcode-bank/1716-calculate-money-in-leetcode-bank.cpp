69e0af8e5f43f999abb67a8be3cee5961e7ab339
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-12-06 16:24:44 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (70.53%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
@@ -5,7 +5,6 @@ public:
         int days = n % 7;
         
         int m = 1 + 2 + 3 + 4 + 5 + 6 + 7;
-        //   = n*m + 7*(0+1+2+..n-1)
         //   = n*m + 7*(n-1)*n/2
         
         int money = weeks*m + 7 * (weeks-1) * weeks / 2;
