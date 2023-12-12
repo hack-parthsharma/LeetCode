b225ce7ef18cd06343c2d3c36a97267e15221eed
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-12-12 07:47:37 +0530

    Time: 0 ms (100.00%), Space: 10.3 MB (38.51%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
@@ -5,7 +5,7 @@ int max1=max(nums[0], nums[1]);
 int max2=min(nums[0], nums[1]);
         
 for (int i=2; i< nums.size(); i++) {
-            if (nums[i] > max1) {
+    if (nums[i] > max1) {
                 max2 = max1;
                 max1 = nums[i];
                 continue;
