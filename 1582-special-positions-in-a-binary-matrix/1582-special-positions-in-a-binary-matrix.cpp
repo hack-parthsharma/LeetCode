3fe8dbcbab3a53b05b87feaaf36746156c5fc779
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-12-13 19:24:24 +0530

    Time: 21 ms (23.42%), Space: 13.1 MB (95.53%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 int numSpecial(vector<vector<int>>& A) {
-        int M = A.size(), N = A[0].size(), ans = 0;
+int M = A.size(), N = A[0].size(), ans = 0;
         for (int i = 0; i < M; ++i) {
             for (int j = 0; j < N; ++j) {
                 if (A[i][j] != 1) continue;
