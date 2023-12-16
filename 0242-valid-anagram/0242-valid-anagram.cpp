480e66b238167f46872f2ee0d47382b510778bc6
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-12-16 21:11:01 +0530

    Time: 7 ms (76.44%), Space: 7.7 MB (79.21%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-  bool isAnagram(string s, string t) {
+bool isAnagram(string s, string t) {
     
 if (s.length() != t.length())
       return false;
     vector<int> count(128);
-    for (const char c : s)
+for (const char c : s)
       ++count[c];
     for (const char c : t)
       if (--count[c] < 0)
