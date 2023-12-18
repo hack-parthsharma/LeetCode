c888c145951277f2d0ae24e74eb10cf1095a0f66
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-12-18 07:49:35 +0530

    Time: 24 ms (47.05%), Space: 20.7 MB (28.86%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cpp
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cpp
@@ -10,7 +10,7 @@ for (const int num : nums) {
       if (num > max1) {
         max2 = max1;
         max1 = num;
-      } else if (num > max2) {
+    } else if (num > max2) {
         max2 = num;
 }
       if (num < min1) {
@@ -21,6 +21,6 @@ for (const int num : nums) {
       }
 }
 
-    return max1 * max2 - min1 * min2;
+return max1 * max2 - min1 * min2;
 }
 };
