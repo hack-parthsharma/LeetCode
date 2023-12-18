8b652080212efa3e390af44197c382bc20dde02f
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-12-18 07:47:45 +0530

    Time: 21 ms (70.62%), Space: 20.5 MB (100.00%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cpp
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cpp
@@ -6,7 +6,7 @@ int max2 = INT_MIN;
 int min1 = INT_MAX;
 int min2 = INT_MAX;
 
-    for (const int num : nums) {
+for (const int num : nums) {
       if (num > max1) {
         max2 = max1;
         max1 = num;
