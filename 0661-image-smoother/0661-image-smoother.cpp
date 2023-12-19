4e1a0108e5688307193d3da7d7e785effe58cb38
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-12-19 18:11:26 +0530

    Time: 42 ms (63.69%), Space: 22 MB (59.38%) - LeetHub

--- 0661-image-smoother/0661-image-smoother.cpp
+++ 0661-image-smoother/0661-image-smoother.cpp
@@ -9,8 +9,8 @@ for (int i = 0; i < m; ++i)
     for (int j = 0; j < n; ++j) {
         int ones = 0;
         int count = 0;
-        for (int x = max(0, i - 1); x < min(m, i + 2); ++x)
-          for (int y = max(0, j - 1); y < min(n, j + 2); ++y) {
+    for (int x = max(0, i - 1); x < min(m, i + 2); ++x)
+         for (int y = max(0, j - 1); y < min(n, j + 2); ++y) {
             ones += M[x][y];
             ++count;
           }
