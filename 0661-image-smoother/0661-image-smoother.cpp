abb33402c33e750be8aba4420276926d63ccc258
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-12-19 18:08:39 +0530

    Time: 36 ms (89.23%), Space: 22.1 MB (33.54%) - LeetHub

--- 0661-image-smoother/0661-image-smoother.cpp
+++ 0661-image-smoother/0661-image-smoother.cpp
@@ -3,9 +3,9 @@ public:
 vector<vector<int>> imageSmoother(vector<vector<int>>& M) {
 const int m = M.size();
 const int n = M[0].size();
-    vector<vector<int>> ans(m, vector<int>(n));
+vector<vector<int>> ans(m, vector<int>(n));
 
-    for (int i = 0; i < m; ++i)
+for (int i = 0; i < m; ++i)
       for (int j = 0; j < n; ++j) {
         int ones = 0;
         int count = 0;
