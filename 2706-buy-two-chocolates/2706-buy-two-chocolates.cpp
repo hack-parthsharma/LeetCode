ca5b4c045c174f54fd42b666332063af82f1753c
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-12-20 07:40:50 +0530

    Time: 11 ms (73.75%), Space: 47 MB (29.77%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 int buyChoco(vector<int>& prices, int money) {
-        sort(prices.begin(), prices.end());
+sort(prices.begin(), prices.end());
         int cost = prices[0] + prices[1];
         return money < cost ? money : money - cost;
     }
