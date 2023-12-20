2b896b5392bc33fa4f334479d66186328341fa46
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-12-20 07:41:14 +0530

    Time: 20 ms (13.52%), Space: 47.1 MB (13.41%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -3,6 +3,6 @@ public:
 int buyChoco(vector<int>& prices, int money) {
 sort(prices.begin(), prices.end());
 int cost = prices[0] + prices[1];
-        return money < cost ? money : money - cost;
+return money < cost ? money : money - cost;
     }
 };
