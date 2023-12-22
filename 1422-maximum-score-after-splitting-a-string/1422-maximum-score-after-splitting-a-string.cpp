72120049261c16c81a98afbcff768deee8a8b7cd
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-12-22 07:47:02 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (43.75%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 int maxScore(string s) {
-        int one = 0, zero = 0, ans = 0;
+int one = 0, zero = 0, ans = 0;
         for (char c : s) one += c == '1';
         for (int i = 0; i < s.size() - 1; ++i) {
             char c  = s[i];
