6d71414751f700ed6bc57b0f1cc3c481d32012ce
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-12-24 14:25:39 +0530

    Time: 6 ms (35.61%), Space: 7.4 MB (45.10%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -4,8 +4,8 @@ int minOperations(string s) {
 int start_with_zero = 0;
 int start_with_one = 0;
 for (int i=0; i<s.size(); i++){
-            if (i % 2 == 0) {
-                s[i] == '1' ? start_with_zero++ : start_with_one++;
+if (i % 2 == 0) {
+s[i] == '1' ? start_with_zero++ : start_with_one++;
             }else{
                 s[i] == '0' ? start_with_zero++ : start_with_one++;
 }
