ed061cb8d6e1e31acc49b9e81d9d442c0e43a3eb
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-12-24 14:24:31 +0530

    Time: 7 ms (8.31%), Space: 7.2 MB (98.52%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
 int minOperations(string s) {
 int start_with_zero = 0;
-        int start_with_one = 0;
+int start_with_one = 0;
         for (int i=0; i<s.size(); i++){
             if (i % 2 == 0) {
                 s[i] == '1' ? start_with_zero++ : start_with_one++;
