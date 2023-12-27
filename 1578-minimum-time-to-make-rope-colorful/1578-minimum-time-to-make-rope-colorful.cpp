d982ed5702f62aadfccc71ebc70c4cd57cbd0f8c
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-12-27 07:47:28 +0530

    Time: 119 ms (58.12%), Space: 95.8 MB (49.48%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -6,7 +6,7 @@ for (int i = 0; i + 1 < N; ++i) {
 if (s[i] != s[i + 1]) continue;
 if (cost[i] > cost[i + 1]) swap(cost[i], cost[i + 1]);
 ans += cost[i];
-        }
+}
         return ans;
     }
 };
