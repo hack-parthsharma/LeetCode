ef835998a13ef2793705309f39743e45770900e9
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2023-12-27 07:46:32 +0530

    Time: 111 ms (79.88%), Space: 95.7 MB (83.31%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
 int minCost(string s, vector<int>& cost) {
 int N = s.size(), ans = 0;
-        for (int i = 0; i + 1 < N; ++i) {
+for (int i = 0; i + 1 < N; ++i) {
             if (s[i] != s[i + 1]) continue;
             if (cost[i] > cost[i + 1]) swap(cost[i], cost[i + 1]);
             ans += cost[i];
