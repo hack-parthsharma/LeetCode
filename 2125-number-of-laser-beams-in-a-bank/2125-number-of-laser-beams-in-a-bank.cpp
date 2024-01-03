36ec15095e9198e5657e74a1b1b8b7dc0f335c69
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-01-03 07:39:52 +0530

    Time: 143 ms (8.87%), Space: 23.4 MB (82.87%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -3,7 +3,6 @@ class Solution {
   int numberOfBeams(vector<string>& bank) {
     int ans = 0;
     int prevOnes = 0;
-
     for (const string& row : bank) {
       const int ones = count(begin(row), end(row), '1');
       if (ones > 0) {
