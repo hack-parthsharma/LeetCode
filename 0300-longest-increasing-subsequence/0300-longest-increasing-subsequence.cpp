dc77a060fb1ab1f2248493c80fabe7cc67f93faa
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-01-05 08:42:18 +0530

    Time: 644 ms (21.09%), Space: 10.7 MB (80.76%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -3,7 +3,7 @@ public:
 int lengthOfLIS(vector<int>& nums) {
 if (nums.empty())
       return 0;
-    vector<int> dp(nums.size(), 1);
+vector<int> dp(nums.size(), 1);
     for (int i = 1; i < nums.size(); ++i)
       for (int j = 0; j < i; ++j)
         if (nums[j] < nums[i])
