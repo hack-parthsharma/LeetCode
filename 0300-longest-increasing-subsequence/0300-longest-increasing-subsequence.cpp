60ce5399380591676f0fe2668c1832c445465c2b
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-01-05 08:45:48 +0530

    Time: 236 ms (59.71%), Space: 11.1 MB (32.33%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -6,7 +6,7 @@ if (nums.empty())
 vector<int> dp(nums.size(), 1);
 for (int i = 1; i < nums.size(); ++i)
     for (int j = 0; j < i; ++j)
-        if (nums[j] < nums[i])
+       if (nums[j] < nums[i])
           dp[i] = max(dp[i], dp[j] + 1);
 return *max_element(begin(dp), end(dp));
 }
