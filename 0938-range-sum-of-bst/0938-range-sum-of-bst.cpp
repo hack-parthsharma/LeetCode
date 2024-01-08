e20db2d96d02be8cd41502f36c62d1ad15ccd59a
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-01-08 08:56:05 +0530

    Time: 102 ms (42.94%), Space: 65 MB (51.38%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
@@ -7,7 +7,7 @@ if (root->val < L)
       return rangeSumBST(root->right, L, R);
 if (root->val > R)
       return rangeSumBST(root->left, L, R);
-    return root->val + rangeSumBST(root->left, L, R) +
+return root->val + rangeSumBST(root->left, L, R) +
            rangeSumBST(root->right, L, R);
 }
 };
