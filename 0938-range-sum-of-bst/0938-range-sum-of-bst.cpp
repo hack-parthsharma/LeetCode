1c565c06c52c50fbb678b4cefe37957d450e8e67
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-01-08 08:55:38 +0530

    Time: 93 ms (76.20%), Space: 65 MB (25.88%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
@@ -3,7 +3,7 @@ public:
 int rangeSumBST(TreeNode* root, int L, int R) {
 if (root == nullptr)
       return 0;
-    if (root->val < L)
+if (root->val < L)
       return rangeSumBST(root->right, L, R);
     if (root->val > R)
       return rangeSumBST(root->left, L, R);
