9d3d33af53a66d0e92e651c0c2c6ec160a422b8f
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-01-08 08:55:48 +0530

    Time: 91 ms (81.54%), Space: 64.9 MB (51.38%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
@@ -5,7 +5,7 @@ if (root == nullptr)
       return 0;
 if (root->val < L)
       return rangeSumBST(root->right, L, R);
-    if (root->val > R)
+if (root->val > R)
       return rangeSumBST(root->left, L, R);
     return root->val + rangeSumBST(root->left, L, R) +
            rangeSumBST(root->right, L, R);
