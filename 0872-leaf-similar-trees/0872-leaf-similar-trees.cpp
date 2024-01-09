e330196fb71806f758e34eddc7768c990cd42b41
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-01-09 08:05:16 +0530

    Time: 3 ms (66.12%), Space: 13.2 MB (77.45%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
@@ -3,7 +3,7 @@ public:
   
 bool leafSimilar(TreeNode* root1, TreeNode* root2) {
 vector<int> leaves1;
-    vector<int> leaves2;
+vector<int> leaves2;
     dfs(root1, leaves1);
     dfs(root2, leaves2);
     return leaves1 == leaves2;
