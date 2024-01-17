4a7ebfc7e29cf767de5b1a9c5d489b5200e5d895
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-01-17 08:47:14 +0530

    Time: 3 ms (54.98%), Space: 8.6 MB (41.08%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
 bool uniqueOccurrences(vector<int>& arr) {
 unordered_map<int, int> count;
-    unordered_set<int> occurrences;
+unordered_set<int> occurrences;
 
-    for (const int a : arr)
+for (const int a : arr)
       ++count[a];
 
     for (const auto& [_, value] : count)
