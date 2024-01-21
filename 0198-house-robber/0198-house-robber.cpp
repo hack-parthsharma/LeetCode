6d96f6bc62a2b2c991c0701c4600355d129deffb
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-01-21 14:54:44 +0530

    Time: 4 ms (25.45%), Space: 9.2 MB (9.67%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -6,7 +6,7 @@ if (nums.empty())
 if (nums.size() == 1)
       return nums[0];
 vector<int> dp(nums.size());
-    dp[0] = nums[0];
+dp[0] = nums[0];
     dp[1] = max(nums[0], nums[1]);
     for (int i = 2; i < nums.size(); ++i)
       dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
