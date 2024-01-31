42bf010410b6d1cd5833fa8296361cbe90fdc3ff
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-01-31 07:54:09 +0530

    Time: 113 ms (95.36%), Space: 101 MB (64.78%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -3,7 +3,7 @@ public:
 vector<int> dailyTemperatures(vector<int>& temperatures) {
 int n = temperatures.size();
 vector<int> ans(n);
-        stack<int> stk;
+stack<int> stk;
         for (int i = n - 1; ~i; --i) {
             while (!stk.empty() && temperatures[stk.top()] <= temperatures[i]) stk.pop();
             if (!stk.empty()) ans[i] = stk.top() - i;
