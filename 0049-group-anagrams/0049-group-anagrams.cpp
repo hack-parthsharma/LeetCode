3a4b312503aaf156ac77a2d4c224479d080bef19
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-02-06 08:34:53 +0530

    Time: 16 ms (98.78%), Space: 23.4 MB (32.97%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,11 +3,11 @@ public:
 vector<vector<string>> groupAnagrams(vector<string>& strs) {
 vector<vector<string>> ans;
 unordered_map<string, vector<string>> keyToAnagrams;
-    for (const string& str : strs) {
+for (const string& str : strs) {
       string key = str;
       sort(begin(key), end(key));
       keyToAnagrams[key].push_back(str);
-    }
+}
     for (const auto& [_, anagrams] : keyToAnagrams)
       ans.push_back(anagrams);
     return ans;
