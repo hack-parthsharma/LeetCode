988a80ea09c08fbac2849b3db117e1fa77f04d62
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-02-08 07:38:43 +0530

    Time: 44 ms (88.00%), Space: 10.5 MB (38.33%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -4,7 +4,8 @@ int numSquares(int n) {
 vector<int> dp(n + 1, n);  
 dp[0] = 0;  
 dp[1] = 1;  
-    for (int i = 2; i <= n; ++i)
+    
+for (int i = 2; i <= n; ++i)
       for (int j = 1; j * j <= i; ++j)
         dp[i] = min(dp[i], dp[i - j * j] + 1);
     return dp[n];
