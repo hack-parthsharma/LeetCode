b081632df21ffbc0a36d5982714dbc511aa6f1f6
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-02-08 07:38:52 +0530

    Time: 45 ms (87.32%), Space: 10.6 MB (29.72%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -6,7 +6,7 @@ dp[0] = 0;
 dp[1] = 1;  
     
 for (int i = 2; i <= n; ++i)
-      for (int j = 1; j * j <= i; ++j)
+   for (int j = 1; j * j <= i; ++j)
         dp[i] = min(dp[i], dp[i - j * j] + 1);
     return dp[n];
 }
