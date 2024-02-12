7dfb1631054624e1d9d89d4bab0d1482393acb87
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-02-12 08:42:10 +0530

    Time: 4 ms (98.82%), Space: 22.1 MB (18.20%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,7 +3,7 @@ public:
 int majorityElement(vector<int>& nums) {
     int ans;
     int count = 0;
-    for (const int num : nums) {
+for (const int num : nums) {
       if (count == 0)
         ans = num;
       count += num == ans ? 1 : -1;
