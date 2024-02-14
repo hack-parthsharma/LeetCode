db447f10e3c6e9d1428c1c8b90c500222163d94c
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-02-14 13:27:44 +0530

    Time: 131 ms (94.75%), Space: 143 MB (6.50%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -8,7 +8,7 @@ public:
 for (const int num : nums)
       (num > 0 ? pos : neg).push_back(num);
 
-    for (int i = 0; i < pos.size(); ++i) {
+for (int i = 0; i < pos.size(); ++i) {
       ans.push_back(pos[i]);
       ans.push_back(neg[i]);
     }
