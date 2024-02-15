9711866e5d93485843f738fd69fbb0f9206c6098
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-02-15 08:42:31 +0530

    Time: 127 ms (96.45%), Space: 82.3 MB (36.63%) - LeetHub

--- 2971-find-polygon-with-the-largest-perimeter/2971-find-polygon-with-the-largest-perimeter.cpp
+++ 2971-find-polygon-with-the-largest-perimeter/2971-find-polygon-with-the-largest-perimeter.cpp
@@ -7,7 +7,6 @@ class Solution {
 
     for (int i = nums.size() - 1; i >= 2; --i) {
       prefix -= nums[i];
-      // Let nums[i] be the longest side. Check if the sum of all the edges with
       // length no longer than nums[i] > nums[i].
       if (prefix > nums[i])
         return prefix + nums[i];
