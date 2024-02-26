2c51dd2862ca0b458072b55f4a0a6589caf54036
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-02-26 09:03:01 +0530

    Time: 0 ms (100.00%), Space: 11.7 MB (5.64%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -3,7 +3,7 @@ public:
 bool isSameTree(TreeNode* p, TreeNode* q) {
 if (!p || !q)
       return p == q;
-    return p->val == q->val &&              //
+return p->val == q->val &&              //
            isSameTree(p->left, q->left) &&  //
            isSameTree(p->right, q->right);
   }
