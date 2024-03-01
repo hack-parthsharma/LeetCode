fac19a7a46ea0c704b8fe43d9763554a907a2790
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-03-01 07:54:14 +0530

    Time: 4 ms (61.13%), Space: 8.6 MB (40.79%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.cpp
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.cpp
@@ -6,7 +6,7 @@ public:
         for (int i = 1; i < cnt; ++i) {
             ans.push_back('1');
         }
-        for (int i = 0; i < s.size() - cnt; ++i) {
+for (int i = 0; i < s.size() - cnt; ++i) {
             ans.push_back('0');
         }
         ans.push_back('1');
