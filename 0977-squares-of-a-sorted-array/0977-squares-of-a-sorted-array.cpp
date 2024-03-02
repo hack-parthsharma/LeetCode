9827ed3ded10869588b6b61d913a64bbdf50cdc2
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-03-02 13:24:47 +0530

    Time: 14 ms (95.59%), Space: 28.3 MB (67.40%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -5,7 +5,7 @@ const int n = nums.size();
     vector<int> ans(n);
     int i = n - 1;
 
-    for (int l = 0, r = n - 1; l <= r;)
+for (int l = 0, r = n - 1; l <= r;)
       if (abs(nums[l]) > abs(nums[r]))
         ans[i--] = nums[l] * nums[l++];
       else
