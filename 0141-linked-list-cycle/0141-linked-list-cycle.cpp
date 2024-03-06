07397a6b8f65e61ab6868e513b88f64fade71898
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-03-06 08:42:35 +0530

    Time: 3 ms (99.20%), Space: 10.4 MB (74.71%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -8,7 +8,7 @@ ListNode* slow = head;
 ListNode* fast = head;
    
 while (fast && fast->next) {
-      slow = slow->next;
+slow = slow->next;
       fast = fast->next->next;
       if (slow == fast)
         return true;
