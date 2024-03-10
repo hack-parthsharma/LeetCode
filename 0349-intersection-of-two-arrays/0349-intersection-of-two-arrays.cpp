baa3850c5c81f57b044e2cac82a2baecfa0f5659
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-03-10 20:06:33 +0530

    Time: 0 ms (100.00%), Space: 12.9 MB (35.37%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -4,7 +4,7 @@ public:
     vector<int> ans;
     unordered_set<int> set{begin(nums1), end(nums1)};
 
-    for (const int num : nums2)
+for (const int num : nums2)
       if (set.erase(num))
         ans.push_back(num);
 
