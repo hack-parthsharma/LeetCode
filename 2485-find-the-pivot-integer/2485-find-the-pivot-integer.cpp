59511529832aeac8ff2e10df9b89cf5dfb45ff7a
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-03-13 08:39:59 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (87.40%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,7 +1,6 @@
 class Solution {
  public:
   int pivotInteger(int n) {
-    //         x + x^2 = nx - x^2 + x + n^2 - nx + n
     //         2 * x^2 = n^2 + n
     //               x = sqrt((n^2 + n) / 2)
     const int y = (n * n + n) / 2;
