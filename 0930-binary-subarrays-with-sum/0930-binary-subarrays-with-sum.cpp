67cfd91940545799c92c5e66b6cae2414653b989
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-03-14 08:34:04 +0530

    Time: 29 ms (62.84%), Space: 38.4 MB (17.14%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -5,7 +5,7 @@ public:
   int prefix = 0;
   unordered_map<int, int> count{{0, 1}};
 
-    for (int num : nums) {
+for (int num : nums) {
       prefix += num;
       if (const auto it = count.find(prefix - goal); it != cend(count))
         ans += it->second;
