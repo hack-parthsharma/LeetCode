4098fe21222334d7fff2831e34dbd375d14dc6b4
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-03-15 17:27:18 +0530

    Time: 14 ms (74.02%), Space: 27.5 MB (23.95%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -9,7 +9,7 @@ for (int i = 1; i < n; ++i)
       prefix[i] = prefix[i - 1] * nums[i - 1];
 for (int i = n - 2; i >= 0; --i)
       suffix[i] = suffix[i + 1] * nums[i + 1];
-    for (int i = 0; i < n; ++i)
+for (int i = 0; i < n; ++i)
       ans[i] = prefix[i] * suffix[i];
     return ans;
   }
