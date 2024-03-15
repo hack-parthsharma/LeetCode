a342ad0984e46812e6ec29d512d9f4bbc9dea8b8
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-03-15 17:26:54 +0530

    Time: 15 ms (67.47%), Space: 27.5 MB (23.95%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -5,7 +5,7 @@ public:
     vector<int> ans(n);        
     vector<int> prefix(n, 1);  
     vector<int> suffix(n, 1); 
-    for (int i = 1; i < n; ++i)
+for (int i = 1; i < n; ++i)
       prefix[i] = prefix[i - 1] * nums[i - 1];
     for (int i = n - 2; i >= 0; --i)
       suffix[i] = suffix[i + 1] * nums[i + 1];
