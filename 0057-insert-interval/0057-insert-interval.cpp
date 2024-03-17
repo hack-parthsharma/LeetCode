6065eef4f9fcce170107cfb9612de49471aea63b
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-03-17 06:13:01 +0530

    Time: 7 ms (92.69%), Space: 20.3 MB (93.49%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -13,7 +13,7 @@ while (i < n && intervals[i][0] <= newInterval[1]) {
       newInterval[0] = min(newInterval[0], intervals[i][0]);
       newInterval[1] = max(newInterval[1], intervals[i][1]);
       ++i;
-    }
+}
 
     ans.push_back(newInterval);
 
