297f9776bd4b822eceecc3ec7a6af6083d5bc552
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-03-17 06:15:30 +0530

    Time: 65 ms (95.78%), Space: 87.7 MB (54.17%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.cpp
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -5,13 +5,13 @@ public:
     int prefix = 0;
     unordered_map<int, int> prefixToIndex{{0, -1}};
 
-    for (int i = 0; i < nums.size(); ++i) {
+for (int i = 0; i < nums.size(); ++i) {
       prefix += nums[i] ? 1 : -1;
       if (const auto it = prefixToIndex.find(prefix); it != cend(prefixToIndex))
         ans = max(ans, i - it->second);
       else
         prefixToIndex[prefix] = i;
-    }
+}
 
     return ans;
   }
