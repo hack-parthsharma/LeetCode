eea8b532ee7d56d0cfc41f75d0ef653ec8a8eefe
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-03-17 06:12:30 +0530

    Time: 14 ms (38.68%), Space: 20.5 MB (58.08%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -9,7 +9,6 @@ public:
     while (i < n && intervals[i][1] < newInterval[0])
       ans.push_back(intervals[i++]);
 
-    // Merge overlapping intervals
     while (i < n && intervals[i][0] <= newInterval[1]) {
       newInterval[0] = min(newInterval[0], intervals[i][0]);
       newInterval[1] = max(newInterval[1], intervals[i][1]);
