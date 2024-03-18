1d0dc8ab26226efeb80975f2c837c57ca2aec585
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-03-18 09:59:26 +0530

    Time: 256 ms (59.95%), Space: 93.3 MB (41.78%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -5,7 +5,7 @@ public:
          [](const auto& a, const auto& b) { return a[1] < b[1]; });
     int ans = 1;
     int arrowX = points[0][1];
-    for (int i = 1; i < points.size(); ++i)
+for (int i = 1; i < points.size(); ++i)
       if (points[i][0] > arrowX) {
         arrowX = points[i][1];
         ++ans;
