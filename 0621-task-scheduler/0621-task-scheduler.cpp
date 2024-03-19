9534a716d031312b98a9d8b3e7cfa68bffd7a930
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-03-19 08:48:40 +0530

    Time: 50 ms (81.67%), Space: 37.7 MB (98.52%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -12,7 +12,6 @@ class Solution {
     const int maxFreq = *max_element(begin(count), end(count));
     const int maxFreqTaskOccupy = (maxFreq - 1) * (n + 1);
     const int nMaxFreq = std::count(begin(count), end(count), maxFreq);
-    // Max(
     //   the most frequent task is frequent enough to force some idle slots,
     //   the most frequent task is not frequent enough to force idle slots
     // )
