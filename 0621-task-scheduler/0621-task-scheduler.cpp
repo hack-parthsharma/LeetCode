162989adee5c6592d8a1f6e72417f579b7e10c18
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-03-19 08:51:31 +0530

    Time: 45 ms (90.28%), Space: 38.1 MB (65.52%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -11,7 +11,7 @@ for (const char task : tasks)
 
 const int maxFreq = *max_element(begin(count), end(count));
 const int maxFreqTaskOccupy = (maxFreq - 1) * (n + 1);
-    const int nMaxFreq = std::count(begin(count), end(count), maxFreq);
+const int nMaxFreq = std::count(begin(count), end(count), maxFreq);
     return max(maxFreqTaskOccupy + nMaxFreq, static_cast<int>(tasks.size()));
 
 }
