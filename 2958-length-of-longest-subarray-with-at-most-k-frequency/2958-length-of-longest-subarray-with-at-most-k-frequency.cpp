c0c1265c4cbe9b44118bebe41e906f625e515bb1
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-03-28 08:03:43 +0530

    Time: 222 ms (40.83%), Space: 148.6 MB (19.98%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -4,7 +4,7 @@ public:
     int ans = 0;
     unordered_map<int, int> count;
 
-    for (int l = 0, r = 0; r < nums.size(); ++r) {
+for (int l = 0, r = 0; r < nums.size(); ++r) {
       ++count[nums[r]];
       while (count[nums[r]] == k + 1)
         --count[nums[l++]];
