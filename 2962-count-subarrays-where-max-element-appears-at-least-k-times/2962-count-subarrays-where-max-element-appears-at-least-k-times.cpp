5eaecf25ba5f09c7f4e338bc57264a091517808d
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-03-29 07:50:01 +0530

    Time: 124 ms (78.29%), Space: 120 MB (62.40%) - LeetHub

--- 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.cpp
+++ 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.cpp
@@ -8,7 +8,6 @@ class Solution {
     for (int l = 0, r = 0; r < nums.size(); ++r) {
       if (nums[r] == maxNum)
         ++count;
-      // Keep the window to include k - 1 times of the maximum number.
       while (count == k)
         if (nums[l++] == maxNum)
           --count;
