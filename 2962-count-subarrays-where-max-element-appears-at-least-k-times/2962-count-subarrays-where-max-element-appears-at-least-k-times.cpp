599dd8a8d11e180c381e45fce0c0e0cb07ee4ce9
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-03-29 07:50:13 +0530

    Time: 123 ms (80.62%), Space: 120.1 MB (62.40%) - LeetHub

--- 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.cpp
+++ 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.cpp
@@ -11,7 +11,6 @@ class Solution {
       while (count == k)
         if (nums[l++] == maxNum)
           --count;
-      // If l > 0, nums[l..r] has k - 1 times of the maximum number. For any
       // subarray nums[i..r], where i < l, it will have at least k times of the
       // maximum number, since nums[l - 1] equals the maximum number.
       ans += l;
