7b7b28c609fb4d47f90be81667649243a3922a4c
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-03-31 16:25:48 +0530

    Time: 92 ms (46.81%), Space: 82.6 MB (53.99%) - LeetHub

--- 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
+++ 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
@@ -13,7 +13,6 @@ class Solution {
         prevMinKIndex = i;
       if (nums[i] == maxK)
         prevMaxKIndex = i;
-      // any index k in [j + 1, min(prevMinKIndex, prevMaxKIndex)] can be the
       // start of the subarray s.t. nums[k..i] satisfies the conditions
       ans += max(0, min(prevMinKIndex, prevMaxKIndex) - j);
     }
