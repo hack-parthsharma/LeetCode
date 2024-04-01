adadd250b0db8236043b7a6a5ad39625fd184dff
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-04-01 07:49:27 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (20.26%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
   int lengthOfLastWord(string s) {
-    int i = s.length() - 1;
+  int i = s.length() - 1;
 while (i >= 0 && s[i] == ' ')
       --i;
     const int lastIndex = i;
