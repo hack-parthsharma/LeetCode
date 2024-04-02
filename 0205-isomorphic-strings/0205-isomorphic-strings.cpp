08652b21777a381e97c07950c1da92c8fde8533a
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-04-02 09:48:51 +0530

    Time: 3 ms (91.66%), Space: 8.1 MB (94.60%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -4,7 +4,7 @@ public:
     vector<int> charToIndex_s(128);
     vector<int> charToIndex_t(128);
 
-    for (int i = 0; i < s.length(); ++i) {
+for (int i = 0; i < s.length(); ++i) {
       if (charToIndex_s[s[i]] != charToIndex_t[t[i]])
         return false;
       charToIndex_s[s[i]] = i + 1;
