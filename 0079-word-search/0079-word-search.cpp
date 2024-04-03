8308aba0e3fdf8d58b59a1b4026a5b51541232ca
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-04-03 08:40:40 +0530

    Time: 240 ms (78.02%), Space: 9.5 MB (89.06%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
   bool exist(vector<vector<char>>& board, string word) {
 for (int i = 0; i < board.size(); ++i)
-      for (int j = 0; j < board[0].size(); ++j)
+    for (int j = 0; j < board[0].size(); ++j)
         if (dfs(board, word, i, j, 0))
           return true;
     return false;
