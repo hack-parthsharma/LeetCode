6ea7ac776c5f24ef08b8ee9138bfc6d13dfce70e
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-04-03 08:45:08 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (97.65%) - LeetHub

--- 3091-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k/3091-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k.cpp
+++ 3091-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k/3091-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k.cpp
@@ -1,7 +1,6 @@
 class Solution {
  public:
   int minOperations(int k) {
-    // Optimally, `x` should equal to `1 + y`, implying that x^2 >= k, and
     // hence, x >= sqrt(k) and y = ceil(k / x) - 1.
     const int x = ceil(sqrt(k));
     const int y = (k - 1) / x + 1 - 1;  
