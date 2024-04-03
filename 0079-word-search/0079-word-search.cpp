47c0fef87ff66cc322459e79c67f2786c77a82e7
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-04-03 08:40:51 +0530

    Time: 240 ms (78.02%), Space: 9.6 MB (89.06%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -5,7 +5,7 @@ for (int i = 0; i < board.size(); ++i)
     for (int j = 0; j < board[0].size(); ++j)
         if (dfs(board, word, i, j, 0))
           return true;
-    return false;
+return false;
 }
  private:
   bool dfs(vector<vector<char>>& board, const string& word, int i, int j,
