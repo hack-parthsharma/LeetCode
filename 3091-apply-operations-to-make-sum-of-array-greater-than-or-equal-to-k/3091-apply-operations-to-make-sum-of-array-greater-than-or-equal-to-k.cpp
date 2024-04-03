69e770f87ee5e5bc58e993321b86c781bb7c74a1
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-04-03 08:46:29 +0530

    Time: 2 ms (59.88%), Space: 7.3 MB (44.59%) - LeetHub

--- 3091-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k/3091-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k.cpp
+++ 3091-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k/3091-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-  int minOperations(int k) {
+int minOperations(int k) {
     const int x = ceil(sqrt(k));
     const int y = (k - 1) / x + 1 - 1;  
     return x - 1 + y;
