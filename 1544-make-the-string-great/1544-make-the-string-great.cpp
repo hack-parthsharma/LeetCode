479a10867a68906c694c62e1691c7df7e964bfad
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-04-05 07:41:54 +0530

    Time: 4 ms (42.41%), Space: 7.6 MB (89.71%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string makeGood(string s) {
         string ans;
-        for (char c : s) {
+for (char c : s) {
             if (ans.size() && ans.back() != c && toupper(ans.back()) == toupper(c)) ans.pop_back();
             else ans.push_back(c);
         }
