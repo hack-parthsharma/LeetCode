401385df0df3894b6f87c022b2ec3ae98056ce1f
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-04-05 07:42:11 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (89.71%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -5,7 +5,7 @@ public:
 for (char c : s) {
             if (ans.size() && ans.back() != c && toupper(ans.back()) == toupper(c)) ans.pop_back();
             else ans.push_back(c);
-        }
+}
         return ans;
 }
 };
