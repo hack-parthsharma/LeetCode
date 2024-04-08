7bc4921bf8e8df054eeb4aca09cf205058f172d2
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-04-08 09:03:33 +0530

    Time: 2 ms (50.57%), Space: 10.6 MB (95.64%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
@@ -4,7 +4,7 @@ public:
         int st[2] = {0};
         for(auto s: students) {
             st[s]++;
-        }
+}
         int cnt = 0;
         for(auto& san : sandwiches){
             if (st[san] == 0) break;
