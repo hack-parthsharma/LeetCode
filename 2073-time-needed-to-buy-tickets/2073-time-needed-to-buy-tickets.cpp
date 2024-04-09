a674d6f61ad86cbafa03db7d80d61e098f273902
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-04-09 08:52:12 +0530

    Time: 4 ms (41.19%), Space: 9.3 MB (67.56%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -7,7 +7,7 @@ for (int i = 0; i < tickets.size(); ++i)
       
     if (i <= k)
         ans += min(tickets[i], tickets[k]);
-      else
+    else
         ans += min(tickets[i], tickets[k] - 1);
 
     return ans;
