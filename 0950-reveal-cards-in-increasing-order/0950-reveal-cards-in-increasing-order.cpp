a58f871f1909cefb7fb5259271993cc778067cc9
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-04-10 08:49:43 +0530

    Time: 3 ms (67.33%), Space: 10.9 MB (34.66%) - LeetHub

--- 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.cpp
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.cpp
@@ -7,7 +7,7 @@ sort(begin(deck), end(deck), greater<int>());
 deque<int> q{deck[0]};
 
 for (int i = 1; i < deck.size(); ++i) {
-      q.push_front(q.back());
+   q.push_front(q.back());
       q.pop_back();
       q.push_front(deck[i]);
 }
