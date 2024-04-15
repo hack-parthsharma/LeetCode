efd0bb7aa9908348ffaac926d0d06a8d9be953e6
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-04-15 09:29:28 +0530

    Time: 5 ms (26.49%), Space: 10.8 MB (80.41%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
   int sumNumbers(TreeNode* root) {
-    int ans = 0;
+  int ans = 0;
     dfs(root, 0, ans);
     return ans;
 }
 
- private:
+private:
       void dfs(TreeNode* root, int path, int& ans) {
 if (root == nullptr)
       return;
