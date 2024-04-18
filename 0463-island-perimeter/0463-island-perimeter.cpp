79e114998b59bb64181d4373a07ff7cae017aea0
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-04-18 08:52:23 +0530

    Time: 65 ms (82.67%), Space: 100.8 MB (48.27%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -10,7 +10,7 @@ for (int i = 0; i < grid.size(); ++i)
           ++islands;
         if (i - 1 >= 0 && grid[i - 1][j])
             ++neighbors;
-          if (j - 1 >= 0 && grid[i][j - 1])
+        if (j - 1 >= 0 && grid[i][j - 1])
             ++neighbors;
         }
 
