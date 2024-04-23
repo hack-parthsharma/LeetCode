93a647284d6735fe4269b3dd2f7d9810d9d04932
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-04-23 07:56:43 +0530

    Time: 180 ms (14.87%), Space: 92.9 MB (7.09%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.cpp
+++ 0310-minimum-height-trees/0310-minimum-height-trees.cpp
@@ -21,14 +21,14 @@ unordered_map<int, unordered_set<int>> graph;
 while (n > 2) {
       n -= ans.size();
       vector<int> nextLeaves;
-      for (const int leaf : ans) {
+for (const int leaf : ans) {
         const int u = *begin(graph[leaf]);
         graph[u].erase(leaf);
         if (graph[u].size() == 1)
           nextLeaves.push_back(u);
       }
       ans = nextLeaves;
-    }
+}
 
     return ans;
   }
