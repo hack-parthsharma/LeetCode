734e89e063864cfd9a843d751a1dbe6fbedad976
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-04-29 08:46:02 +0530

    Time: 93 ms (94.24%), Space: 91.3 MB (54.80%) - LeetHub

--- 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
+++ 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -1,5 +1,5 @@
 class Solution {
- public:
+public:
   int minOperations(vector<int>& nums, int k) {
     const int xors = accumulate(nums.begin(), nums.end(), 0, bit_xor<>());
     return __builtin_popcount(k ^ xors);
