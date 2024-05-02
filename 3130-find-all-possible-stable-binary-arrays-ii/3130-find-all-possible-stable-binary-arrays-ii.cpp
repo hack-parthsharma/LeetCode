1e60fdf3351ac9f09bb9f05f2ee657fdc4239faf
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-05-02 07:42:42 +0530

    Time: 1273 ms (39.32%), Space: 511.3 MB (33.93%) - LeetHub

--- 3130-find-all-possible-stable-binary-arrays-ii/3130-find-all-possible-stable-binary-arrays-ii.cpp
+++ 3130-find-all-possible-stable-binary-arrays-ii/3130-find-all-possible-stable-binary-arrays-ii.cpp
@@ -3,8 +3,6 @@ class Solution {
   int numberOfStableArrays(int zero, int one, int limit) {
     constexpr int kMod = 1'000'000'007;
     // dp[i][j][k] := the number of stable arrays, where the number of
-    // ocurrences of 0 is i and the number of ocurrences of 1 is j and the last
-    // number is k (0/1)
     vector<vector<vector<long>>> dp(
         zero + 1, vector<vector<long>>(one + 1, vector<long>(2)));
 
