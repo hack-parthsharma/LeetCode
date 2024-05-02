3c3fba5454ab57e0119108a27c55afc98419ec3b
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-05-02 07:36:38 +0530

    Time: 24 ms (33.88%), Space: 26.1 MB (26.52%) - LeetHub

--- 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.cpp
+++ 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.cpp
@@ -4,7 +4,7 @@ public:
     int ans = -1;
     unordered_set<int> seen;
 
-    for (const int num : nums)
+for (const int num : nums)
       if (seen.count(-num))
         ans = max(ans, abs(num));
       else
