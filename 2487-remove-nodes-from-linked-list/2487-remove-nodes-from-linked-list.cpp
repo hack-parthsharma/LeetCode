a8f3378b3726088f021222bc5e431eb0ce986e17
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-05-06 13:01:20 +0530

    Time: 264 ms (73.19%), Space: 164.5 MB (57.95%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.cpp
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.cpp
@@ -4,7 +4,7 @@ ListNode* removeNodes(ListNode* head) {
     if (head == nullptr)
       return nullptr;
 head->next = removeNodes(head->next);
-    return head->next != nullptr && head->val < head->next->val ? head->next
+      return head->next != nullptr && head->val < head->next->val ? head->next
                                                                 : head;
   }
 };
