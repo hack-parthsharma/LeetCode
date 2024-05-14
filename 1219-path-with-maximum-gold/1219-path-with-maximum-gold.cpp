a40738af834c988fcac25a89d6cd948b2e2be33d
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-05-14 15:44:42 +0530

    Time: 225 ms (41.18%), Space: 10.1 MB (28.03%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
@@ -3,7 +3,7 @@ public:
   int getMaximumGold(vector<vector<int>>& grid) {
     int ans = 0;
 
-    for (int i = 0; i < grid.size(); ++i)
+for (int i = 0; i < grid.size(); ++i)
       for (int j = 0; j < grid[0].size(); ++j)
         ans = max(ans, dfs(grid, i, j));
 
