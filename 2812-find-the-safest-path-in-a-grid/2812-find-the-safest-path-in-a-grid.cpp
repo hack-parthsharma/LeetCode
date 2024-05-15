e32a470c93e008c19a96f0556b776a2943f8edc0
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-05-15 07:25:56 +0530

    Time: 759 ms (34.50%), Space: 205.6 MB (38.66%) - LeetHub

--- 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.cpp
+++ 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.cpp
@@ -52,7 +52,7 @@ private:
 
 vector<vector<int>> getDistToThief(const vector<vector<int>>& grid) {
 const int n = grid.size();
-    vector<vector<int>> distToThief(n, vector<int>(n));
+vector<vector<int>> distToThief(n, vector<int>(n));
     queue<pair<int, int>> q;
     vector<vector<bool>> seen(n, vector<bool>(n));
 
