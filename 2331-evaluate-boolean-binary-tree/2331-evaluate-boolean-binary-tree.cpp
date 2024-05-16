ef3becfbf5d55d18e288391315061fbd2f79f42f
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-05-16 07:10:39 +0530

    Time: 13 ms (24.30%), Space: 17.5 MB (24.21%) - LeetHub

--- 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.cpp
+++ 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.cpp
@@ -5,6 +5,6 @@ public:
       return root->val;
     if (root->val == 2)  
       return evaluateTree(root->left) || evaluateTree(root->right);
-    return evaluateTree(root->left) && evaluateTree(root->right);
+      return evaluateTree(root->left) && evaluateTree(root->right);
 }
 };
