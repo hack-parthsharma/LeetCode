17d9494cb40b01d5079d83166f7f5abe2e45b7da
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-05-16 07:16:35 +0530

    Time: 2 ms (62.61%), Space: 7.9 MB (51.86%) - LeetHub

--- 3133-minimum-array-end/3133-minimum-array-end.cpp
+++ 3133-minimum-array-end/3133-minimum-array-end.cpp
@@ -10,7 +10,6 @@ class Solution {
 
     for (int i = 0; i < kMaxBit; ++i) {
       if ((ans >> i & 1) == 0) {
-        // Set x's 0 with k's bit if the running bit of k is 1.
         if ((k >> kBinaryIndex & 1) == 1)
           ans |= 1L << i;
         ++kBinaryIndex;
