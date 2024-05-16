ced6fc79f6e32434ed98b5f2d19e4015c424821f
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-05-16 07:15:20 +0530

    Time: 4 ms (93.30%), Space: 32.2 MB (92.98%) - LeetHub

--- 3132-find-the-integer-added-to-array-ii/3132-find-the-integer-added-to-array-ii.cpp
+++ 3132-find-the-integer-added-to-array-ii/3132-find-the-integer-added-to-array-ii.cpp
@@ -1,7 +1,6 @@
 class Solution {
  public:
   int minimumAddedInteger(vector<int>& nums1, vector<int>& nums2) {
-    // elements removed) and nums2 is represented by nums2[0] - nums1[i], where
     // 0 <= i <= 2.
     int ans = INT_MAX;
 
