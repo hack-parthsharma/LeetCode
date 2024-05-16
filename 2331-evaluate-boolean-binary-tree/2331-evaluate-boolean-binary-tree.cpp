86567d4d0f7f510138ffe98a1d118b7547f0021e
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-05-16 07:10:18 +0530

    Time: 10 ms (63.78%), Space: 17.2 MB (92.60%) - LeetHub

--- 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.cpp
+++ 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.cpp
@@ -3,7 +3,7 @@ public:
   bool evaluateTree(TreeNode* root) {
     if (root->val < 2)
       return root->val;
-    if (root->val == 2)  // OR
+    if (root->val == 2)  
       return evaluateTree(root->left) || evaluateTree(root->right);
     return evaluateTree(root->left) && evaluateTree(root->right);
   }
