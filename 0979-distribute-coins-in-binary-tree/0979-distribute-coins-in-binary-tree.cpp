ca81a6888e8cbf27697a0f7c1af8a8c4b17229e7
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-05-18 07:17:41 +0530

    Time: 0 ms (100.00%), Space: 15.7 MB (22.37%) - LeetHub

--- 0979-distribute-coins-in-binary-tree/0979-distribute-coins-in-binary-tree.cpp
+++ 0979-distribute-coins-in-binary-tree/0979-distribute-coins-in-binary-tree.cpp
@@ -13,7 +13,7 @@ return 0;
 
 const int l = dfs(root->left, ans);
 const int r = dfs(root->right, ans);
-    ans += abs(l) + abs(r);
+ans += abs(l) + abs(r);
     return (root->val - 1) + l + r;
 }
 };
