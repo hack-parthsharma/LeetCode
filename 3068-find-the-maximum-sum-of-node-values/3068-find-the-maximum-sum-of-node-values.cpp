6b064b1603516e54d8cfd23402f6336c08a16489
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-05-19 07:39:21 +0530

    Time: 183 ms (77.11%), Space: 125.7 MB (67.56%) - LeetHub

--- 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.cpp
+++ 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.cpp
@@ -6,11 +6,11 @@ public:
     int changedCount = 0;
     int minChangeDiff = INT_MAX;
 
-    for (const int num : nums) {
+for (const int num : nums) {
       maxSum += max(num, num ^ k);
       changedCount += ((num ^ k) > num) ? 1 : 0;
       minChangeDiff = min(minChangeDiff, abs(num - (num ^ k)));
-    }
+}
 
 if (changedCount % 2 == 0)
       return maxSum;
