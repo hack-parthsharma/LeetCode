fdb2aaed1c7db072e93bb17fe76d7541ec4ebce1
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-05-21 07:50:26 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (62.90%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -10,7 +10,7 @@ private:
     vector<vector<int>>& ans) {
     ans.push_back(path);
 for (int i = s; i < nums.size(); ++i) {
-      path.push_back(nums[i]);
+    path.push_back(nums[i]);
       dfs(nums, i + 1, move(path), ans);
       path.pop_back();
     }
