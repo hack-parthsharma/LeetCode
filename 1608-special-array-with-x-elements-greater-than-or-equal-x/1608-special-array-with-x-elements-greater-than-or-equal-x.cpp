053174f4f942da5f9aad1d5b880b9e9fe92e2f08
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-05-27 07:53:43 +0530

    Time: 0 ms (100.00%), Space: 10.2 MB (19.80%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -3,7 +3,7 @@ public:
 int specialArray(vector<int>& A) {
         sort(begin(A), end(A));
         int N = A.size();
-        for (int i = 0; i <= N; ++i) {
+for (int i = 0; i <= N; ++i) {
             int cnt = N - (lower_bound(begin(A), end(A), i) - begin(A));
             if (cnt == i) return i;
         }
