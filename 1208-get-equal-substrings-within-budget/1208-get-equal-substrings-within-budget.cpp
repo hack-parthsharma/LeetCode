6c7a7ab596e1b9e963cea678a80507b0513ccbba
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-05-28 07:17:29 +0530

    Time: 8 ms (46.85%), Space: 8.7 MB (70.28%) - LeetHub

--- 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.cpp
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.cpp
@@ -6,7 +6,7 @@ for (int i = 0; i < s.length(); ++i) {
       maxCost -= abs(s[i] - t[i]);
       if (maxCost < 0)
         maxCost += abs(s[j] - t[j++]);
-    }
+}
 
     return s.length() - j;
   }
