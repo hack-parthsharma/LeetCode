3983317084201ccca7011c5d3e08832f5dc88fd9
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-05-30 07:47:05 +0530

    Time: 86 ms (85.35%), Space: 47.7 MB (62.37%) - LeetHub

--- 3149-find-the-minimum-cost-array-permutation/3149-find-the-minimum-cost-array-permutation.java
+++ 3149-find-the-minimum-cost-array-permutation/3149-find-the-minimum-cost-array-permutation.java
@@ -2,7 +2,7 @@ class Solution {
 private Integer[][] f;
 private int[] nums;
 private int[] ans;
-    private int n;
+private int n;
 
     public int[] findPermutation(int[] nums) {
         n = nums.length;
