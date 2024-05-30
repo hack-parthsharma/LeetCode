088444553bd1e4f181ff9ce9e4932c60b27c2dac
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-05-30 07:43:36 +0530

    Time: 134 ms (42.86%), Space: 61.9 MB (96.43%) - LeetHub

--- 3148-maximum-difference-score-in-a-grid/3148-maximum-difference-score-in-a-grid.ts
+++ 3148-maximum-difference-score-in-a-grid/3148-maximum-difference-score-in-a-grid.ts
@@ -2,7 +2,7 @@ function maxScore(grid: number[][]): number {
 const [m, n] = [grid.length, grid[0].length];
 const f: number[][] = Array.from({ length: m }, () => Array.from({ length: n }, () => 0));
 let ans = -Infinity;
-    for (let i = 0; i < m; ++i) {
+for (let i = 0; i < m; ++i) {
         for (let j = 0; j < n; ++j) {
             let mi = Infinity;
             if (i) {
