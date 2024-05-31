a3b7b011dfd93a82a45752fa9a880f27a8c54a73
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-05-31 08:02:28 +0530

    Time: 9 ms (44.18%), Space: 12.4 MB (64.77%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -3,7 +3,6 @@ public:
     vector<int> singleNumber(vector<int>& nums) {
         int xor_all = 0;
         for(int num: nums) xor_all ^= num;
-        // xor_all will contain a^b, where a and b are repeated only once.
         // if value of a bit in xor is 1, then it means either a or b has 
         // 1 in that position, but not both. We can use this to find the answer.
         int setbit = 1;
