4e2cd8f15df0aee90446f34b33bb849e47a59a17
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-06-05 08:02:00 +0530

    Time: 9 ms (50.60%), Space: 11.4 MB (60.89%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.cpp
+++ 1002-find-common-characters/1002-find-common-characters.cpp
@@ -10,9 +10,9 @@ for (const string& a : A) {
         ++count[c - 'a'];
       for (int i = 0; i < 26; ++i)
         commonCount[i] = min(commonCount[i], count[i]);
-    }
+}
 
-    for (char c = 'a'; c <= 'z'; ++c)
+for (char c = 'a'; c <= 'z'; ++c)
       for (int i = 0; i < commonCount[c - 'a']; ++i)
         ans.push_back(string(1, c));
 
