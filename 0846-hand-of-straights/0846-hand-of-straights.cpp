2354e5d89fe631e5afeb18e1aa2a01caec024b19
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-06-06 08:04:39 +0530

    Time: 54 ms (73.73%), Space: 30.7 MB (42.24%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.cpp
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -9,7 +9,8 @@ for (const int card : hand)
 for (const auto& [start, _] : count) {
       const int value = count[start];
 if (value > 0)
-        for (int i = start; i < start + groupSize; ++i) {
+
+for (int i = start; i < start + groupSize; ++i) {
           count[i] -= value;
           if (count[i] < 0)
             return false;
