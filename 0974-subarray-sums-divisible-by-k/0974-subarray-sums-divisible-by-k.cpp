17adf1d19f93e6b2e4d36b50f7aa5bc819e76363
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-06-09 07:29:56 +0530

    Time: 32 ms (84.09%), Space: 32.6 MB (86.55%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
@@ -3,7 +3,7 @@ public:
      int subarraysDivByK(vector<int>& nums, int k) {
      int ans = 0;
      int prefix = 0;
-    vector<int> count(k);
+     vector<int> count(k);
     count[0] = 1;
 
     for (const int num : nums) {
