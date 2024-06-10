d12e74411a4d14aa31783026c9be67757725a27c
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-06-10 07:38:12 +0530

    Time: 0 ms (100.00%), Space: 10 MB (96.49%) - LeetHub

--- 1051-height-checker/1051-height-checker.cpp
+++ 1051-height-checker/1051-height-checker.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
   int heightChecker(vector<int>& heights) {
   int ans = 0;
-    int currentHeight = 1;
+  int currentHeight = 1;
     vector<int> count(101);
 
     for (int height : heights)
