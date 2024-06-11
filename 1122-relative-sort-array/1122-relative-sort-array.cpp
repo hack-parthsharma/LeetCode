60547d92139244fa7aff9cee449df52e885204eb
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-06-11 07:37:29 +0530

    Time: 2 ms (61.59%), Space: 9.5 MB (71.58%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.cpp
+++ 1122-relative-sort-array/1122-relative-sort-array.cpp
@@ -11,7 +11,7 @@ for (int a : arr2)
       while (count[a]-- > 0)
         ans.push_back(a);
 
-    for (int num = 0; num < 1001; ++num)
+for (int num = 0; num < 1001; ++num)
       while (count[num]-- > 0)
         ans.push_back(num);
 
