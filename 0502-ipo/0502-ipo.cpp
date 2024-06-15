3ca2b79bd6af3eaed57a7c3202c572b5c1499bcc
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-06-15 07:56:55 +0530

    Time: 163 ms (26.94%), Space: 87.7 MB (21.90%) - LeetHub

--- 0502-ipo/0502-ipo.cpp
+++ 0502-ipo/0502-ipo.cpp
@@ -12,7 +12,7 @@ public:
     priority_queue<T, vector<T>, decltype(compareP)> maxHeap(compareP);
     for (int i = 0; i < Capital.size(); ++i)
     minHeap.emplace(Profits[i], Capital[i]);
-    while (k--) {
+while (k--) {
       while (!minHeap.empty() && minHeap.top().cap <= W)
         maxHeap.push(minHeap.top()), minHeap.pop();
       if (maxHeap.empty())
