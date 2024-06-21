c0a7929e4deadf8e0a004b9a033e4aa145de02cb
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-06-21 07:48:08 +0530

    Time: 24 ms (59.49%), Space: 34.2 MB (82.09%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-  int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int X) {
+    int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int X) {
     int satisfied = 0;
     int madeSatisfied = 0;
     int windowSatisfied = 0;
 
-    for (int i = 0; i < customers.size(); ++i) {
+for (int i = 0; i < customers.size(); ++i) {
       if (grumpy[i] == 0)
         satisfied += customers[i];
       else
