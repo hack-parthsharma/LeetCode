b123793a8e9d42ae64a1724be5f306aea076d126
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-06-26 07:32:48 +0530

    Time: 91 ms (57.31%), Space: 62.6 MB (40.07%) - LeetHub

--- 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
@@ -19,6 +19,6 @@ TreeNode* build(const vector<int>& nums, int l, int r) {
 if (l > r)
 return nullptr;
 const int m = (l + r) / 2;
-    return new TreeNode(nums[m], build(nums, l, m - 1), build(nums, m + 1, r));
+return new TreeNode(nums[m], build(nums, l, m - 1), build(nums, m + 1, r));
   }
 };
