1df3cb16653bd2e506d74e75c17528c19d1a4fe2
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-07-01 08:17:52 +0530

    Time: 0 ms (100.00%), Space: 10.6 MB (45.76%) - LeetHub

--- 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
@@ -4,7 +4,7 @@ public:
         int cnt = 0;
         for (auto n : arr) {
             if ( n % 2 ) cnt++;
-            else cnt = 0;
+        else cnt = 0;
             if (cnt >=3) return true;
         }
         return false;
