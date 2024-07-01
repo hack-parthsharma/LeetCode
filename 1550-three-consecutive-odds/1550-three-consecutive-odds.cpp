7b7e1d41a5297ff7d282dd0ded9df66f9b094a62
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-07-01 08:18:27 +0530

    Time: 6 ms (25.20%), Space: 10.6 MB (45.76%) - LeetHub

--- 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
         bool threeConsecutiveOdds(vector<int>& arr) {
         int cnt = 0;
-        for (auto n : arr) {
+for (auto n : arr) {
         if ( n % 2 ) cnt++;
         else cnt = 0;
         if (cnt >=3) return true;
