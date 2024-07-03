622db07f4e326b5960d5252a7236dbf755e6293f
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-07-03 07:36:11 +0530

    Time: 73 ms (34.80%), Space: 38.6 MB (90.81%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -10,7 +10,7 @@ int ans = INT_MAX;
 sort(begin(nums), end(nums));
 
 for (int i = 0; i <= 3; ++i)
-      ans = min(ans, nums[n - 4 + i] - nums[i]);
+ans = min(ans, nums[n - 4 + i] - nums[i]);
 
     return ans;
   }
