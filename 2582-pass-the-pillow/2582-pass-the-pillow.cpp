f1d67bab19a322b6155ff7bf11df3b03d32a6695
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-07-06 07:39:01 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (30.58%) - LeetHub

--- 2582-pass-the-pillow/2582-pass-the-pillow.cpp
+++ 2582-pass-the-pillow/2582-pass-the-pillow.cpp
@@ -2,7 +2,7 @@ class Solution {
  public:
   int passThePillow(int n, int time) {
     time %= (n - 1) * 2;
-    if (time < n)  // Going forward from 1.
+    if (time < n)  
       return 1 + time;
     return n - (time - (n - 1));  // Going backward from n.
   }
