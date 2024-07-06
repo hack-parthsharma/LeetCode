70ed26042b01f1b35ce600066e5bc086a6db5772
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-07-06 07:40:04 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (30.58%) - LeetHub

--- 2582-pass-the-pillow/2582-pass-the-pillow.cpp
+++ 2582-pass-the-pillow/2582-pass-the-pillow.cpp
@@ -3,7 +3,7 @@ class Solution {
   int passThePillow(int n, int time) {
     time %= (n - 1) * 2;
     if (time < n)  
-      return 1 + time;
+    return 1 + time;
     return n - (time - (n - 1));  
 }
 };
