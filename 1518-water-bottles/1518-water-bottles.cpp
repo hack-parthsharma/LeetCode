7381c190200a58bc75cdae98c98af8b09731507f
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-07-07 07:47:11 +0530

    Time: 2 ms (53.84%), Space: 7 MB (72.30%) - LeetHub

--- 1518-water-bottles/1518-water-bottles.cpp
+++ 1518-water-bottles/1518-water-bottles.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
     int ans = numBottles;
-
 while (numBottles >= numExchange) {
     ans += numBottles / numExchange;
     numBottles = numBottles / numExchange + numBottles % numExchange;
