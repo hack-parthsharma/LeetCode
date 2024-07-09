90134c2af1e37aa186d3fd14b7489c0ea21b78cd
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-07-09 13:54:14 +0530

    Time: 152 ms (52.35%), Space: 76.4 MB (83.82%) - LeetHub

--- 1701-average-waiting-time/1701-average-waiting-time.cpp
+++ 1701-average-waiting-time/1701-average-waiting-time.cpp
@@ -7,7 +7,7 @@ for (auto& e : customers) {
             int a = e[0], b = e[1];
             t = max(t, a) + b;
             tot += t - a;
-        }
+}
         return tot / customers.size();
     }
 };
