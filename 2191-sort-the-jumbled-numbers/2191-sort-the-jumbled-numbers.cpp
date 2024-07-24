85b5f0ff92128849da05ef8feebd004ac8d070ff
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-07-24 07:39:55 +0530

    Time: 433 ms (32.90%), Space: 93.9 MB (41.99%) - LeetHub

--- 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.cpp
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.cpp
@@ -17,7 +17,7 @@ class Solution {
 
 private:
   int getMapped(int num, const vector<int>& mapping) {
-    string mapped;
+  string mapped;
     for (const char c : to_string(num))
       mapped += to_string(mapping[c - '0']);
     return stoi(mapped);
