b4406a4a70da63502a8aec12db9d6ec81c6b3fe5
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-07-26 07:43:36 +0530

    Time: 20 ms (92.28%), Space: 15.6 MB (75.48%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -3,7 +3,7 @@ public:
   int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
   int ans = -1;
   int minCitiesCount = n;
-    const vector<vector<int>> dist = floydWarshall(n, edges, distanceThreshold);
+  const vector<vector<int>> dist = floydWarshall(n, edges, distanceThreshold);
 
     for (int i = 0; i < n; ++i) {
       int citiesCount = 0;
