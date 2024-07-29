0511c597ed4651cf6ae3d9e353b67648aa45fd5a
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-07-29 08:00:27 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (52.28%) - LeetHub

--- 3174-clear-digits/3174-clear-digits.cpp
+++ 3174-clear-digits/3174-clear-digits.cpp
@@ -5,7 +5,6 @@ class Solution {
 
     for (const char c : s)
       if (isdigit(c))
-        // character is equivalent to deleting the closest non-digit character.
         ans.pop_back();
       else
         ans += c;
