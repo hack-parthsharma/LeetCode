334852e560e972aa744116b3121a1bc106371528
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-07-30 07:56:37 +0530

    Time: 21 ms (84.96%), Space: 45.3 MB (77.16%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
@@ -1,7 +1,7 @@
 class Solution {
 public int minimumDeletions(String s) {
 int n = s.length();
-        int ans = 0, b = 0;
+int ans = 0, b = 0;
         for (int i = 0; i < n; ++i) {
             if (s.charAt(i) == 'b') {
                 ++b;
