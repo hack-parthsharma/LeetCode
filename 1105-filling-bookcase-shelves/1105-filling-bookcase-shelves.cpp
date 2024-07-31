a6875ae5a25e222689225954769c37ed28e41d19
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-07-31 07:48:06 +0530

    Time: 0 ms (100.00%), Space: 11 MB (58.66%) - LeetHub

--- 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.cpp
+++ 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-  int minHeightShelves(vector<vector<int>>& books, int shelfWidth) {
+      int minHeightShelves(vector<vector<int>>& books, int shelfWidth) {
   vector<int> dp(books.size() + 1, INT_MAX);
   dp[0] = 0;
 
