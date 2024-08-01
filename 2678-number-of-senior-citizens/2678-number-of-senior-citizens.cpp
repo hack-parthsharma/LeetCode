c38f62a7d054a17e82a0a6e19dd90c2cce6885fc
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-08-01 07:32:46 +0530

    Time: 8 ms (47.01%), Space: 17.7 MB (16.71%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
@@ -1,5 +1,5 @@
 class Solution {
- public:
+public:
   int countSeniors(vector<string>& details) {
     return count_if(details.begin(), details.end(), [](const string& detail) {
       return stoi(detail.substr(11, 2)) > 60;
