8f08840f1e0a6a90c5012f9b64ff9bf3d50a63b1
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-08-03 08:11:41 +0530

    Time: 9 ms (63.29%), Space: 20 MB (14.46%) - LeetHub

--- 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -7,7 +7,7 @@ int len2 = arr.size();
 if (len1 != len2) return false;
 if (len1 == 0) return true;
         
-        unordered_map<int, int> stat;
+unordered_map<int, int> stat;
         for (int i=0; i<len1; i++) {
             stat[target[i]]++;
             stat[arr[i]]--;
