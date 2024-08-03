a16fbbe16a2bcf84503d04d583c021187999d9f2
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-08-03 08:11:08 +0530

    Time: 13 ms (29.25%), Space: 20.1 MB (14.13%) - LeetHub

--- 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -4,7 +4,7 @@ bool canBeEqual(vector<int>& target, vector<int>& arr) {
        
 int len1 = target.size();
 int len2 = arr.size();
-        if (len1 != len2) return false;
+if (len1 != len2) return false;
         if (len1 == 0) return true;
         
         unordered_map<int, int> stat;
