ae37f56d12300663392bb66b9928479c273a6a76
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-08-06 07:53:20 +0530

    Time: 75 ms (51.16%), Space: 24.8 MB (66.15%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
 int minimumPushes(string word) {
 int ans = 0;
-    vector<int> count(26);
+vector<int> count(26);
 
     for (const char c : word)
       ++count[c - 'a'];
