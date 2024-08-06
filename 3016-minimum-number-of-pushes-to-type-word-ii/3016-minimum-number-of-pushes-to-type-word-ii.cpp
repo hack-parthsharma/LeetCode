dc5bf45a70d09a54f8f2a78bdb9d3537763b9444
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-08-06 07:53:41 +0530

    Time: 59 ms (96.78%), Space: 25.1 MB (57.94%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -7,7 +7,7 @@ vector<int> count(26);
 for (const char c : word)
       ++count[c - 'a'];
 
-    ranges::sort(count, greater<>());
+ranges::sort(count, greater<>());
 
     for (int i = 0; i < 26; ++i)
       ans += count[i] * (i / 8 + 1);
