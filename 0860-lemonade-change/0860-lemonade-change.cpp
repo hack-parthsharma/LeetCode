b778686b31a28b01a7d52dc5959312677ce32b87
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-08-16 07:44:10 +0530

    Time: 74 ms (48.42%), Space: 85.9 MB (64.60%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.cpp
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 bool lemonadeChange(vector<int>& bills) {
-        int five = 0;
+int five = 0;
         int ten = 0;
         for (int i = 0 ; i < bills.size() ; i++) {
             if (bills[i] == 5) 
