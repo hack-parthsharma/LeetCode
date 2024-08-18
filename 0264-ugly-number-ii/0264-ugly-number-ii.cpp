dde9a717f05fc4fc7a23c5f9d54877ce69aea888
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-08-18 17:35:15 +0530

    Time: 9 ms (43.57%), Space: 11.7 MB (41.02%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -10,7 +10,7 @@ while (uglyNums.size() < n) {
 const int next2 = uglyNums[i2] * 2;
 const int next3 = uglyNums[i3] * 3;
 const int next5 = uglyNums[i5] * 5;
-      const int next = min({next2, next3, next5});
+const int next = min({next2, next3, next5});
       if (next == next2)
         ++i2;
       if (next == next3)
