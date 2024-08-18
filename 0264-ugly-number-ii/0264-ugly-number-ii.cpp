8a381657f01dd79dd10b97aa81d3783fc16db387
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-08-18 17:34:26 +0530

    Time: 9 ms (43.57%), Space: 11.7 MB (42.92%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -4,7 +4,7 @@ int nthUglyNumber(int n) {
 vector<int> uglyNums{1};
 int i2 = 0;
 int i3 = 0;
-    int i5 = 0;
+int i5 = 0;
 
 while (uglyNums.size() < n) {
       const int next2 = uglyNums[i2] * 2;
