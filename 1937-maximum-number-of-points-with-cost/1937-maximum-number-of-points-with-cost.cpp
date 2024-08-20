073137b2fce06f4474f96ce2fcddb30c778fa907
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-08-20 07:50:01 +0530

    Time: 215 ms (37.59%), Space: 117.2 MB (66.42%) - LeetHub

--- 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.cpp
+++ 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.cpp
@@ -2,7 +2,6 @@ class Solution {
  public:
   long long maxPoints(vector<vector<int>>& points) {
     const int n = points[0].size();
-    // dp[j] := max # points you can have if points[i][j] is the most recent
     vector<long long> dp(n);
 
     for (const vector<int>& row : points) {
