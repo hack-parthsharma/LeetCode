d2dad1bde9dc6bc6c619a8ab59a0db390ffa3762
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-08-30 22:37:47 +0530

    Time: 709 ms (36.05%), Space: 197.6 MB (99.57%) - LeetHub

--- 2699-modify-graph-edge-weights/2699-modify-graph-edge-weights.cpp
+++ 2699-modify-graph-edge-weights/2699-modify-graph-edge-weights.cpp
@@ -1,6 +1,5 @@
 using ll = long long;
 const int inf = 2e9;
-
 class Solution {
 public:
     vector<vector<int>> modifiedGraphEdges(int n, vector<vector<int>>& edges, int source, int destination, int target) {
