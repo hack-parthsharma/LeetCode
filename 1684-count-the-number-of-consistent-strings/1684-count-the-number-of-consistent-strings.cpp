43f99f18f12727fcb984e000930f89590c49343e
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-09-12 20:07:00 +0530

    Time: 44 ms (71.41%), Space: 33.9 MB (58.62%) - LeetHub

--- 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.cpp
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.cpp
@@ -5,7 +5,7 @@ int countConsistentStrings(string allowed, vector<string>& words) {
             int mask = 0;
             for (auto& c : w) mask |= 1 << (c - 'a');
             return mask;
-        };
+};
         int mask = f(allowed);
         int ans = 0;
         for (auto& w : words) ans += (mask | f(w)) == mask;
