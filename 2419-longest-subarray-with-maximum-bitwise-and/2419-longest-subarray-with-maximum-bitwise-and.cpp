13f06330586756bc19b355bb6e251f972031292e
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2024-09-14 20:59:54 +0530

    Time: 110 ms (36.84%), Space: 85 MB (34.21%) - LeetHub

--- 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.cpp
+++ 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.cpp
@@ -8,7 +8,7 @@ int sameNumLength = 0;
 for (int i = 0; i < nums.size(); ++i)
       if (nums[i] == nums[maxIndex]) {
         ans = max(ans, ++sameNumLength);
-      } else if (nums[i] > nums[maxIndex]) {
+} else if (nums[i] > nums[maxIndex]) {
         maxIndex = i;
         sameNumLength = 1;
         ans = 1;
